370bbe0c182574e8d11c62df82d3369a926010fe
Ryan Davis <ryand@zenspider.com>
1999-06-14 18:08:36 -0800

    Started moving towards 21.2's custom features. Not done yet.
    [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 109]

--- emacs.el
+++ emacs.el
@@ -1,37 +1,32 @@
-(setq inhibit-startup-message t) ;; avoid splash screen
-
-;; Add my elisp directory to pathing
+;;; Add my elisp directory to pathing
 (pushnew (expand-file-name "~/Bin/elisp/")
-	 load-path :test 'string=)
-(pushnew (expand-file-name "~/Bin/elisp/cc-mode/")
 	 load-path :test 'string=)
 
 (byte-recompile-directory (expand-file-name "~/Bin/elisp") 0)
 
-;; Load My Stuff
+;;; Load My Stuff
 (load "setup-aliases")
 (load "setup-keys")
 (load "setup-mail-and-news")
 (load "setup-misc")
 (load "setup-modes")
 
-;; Options Menu Settings
-;; =====================
-(cond
- ((and (string-match "XEmacs" emacs-version)
-       (boundp 'emacs-major-version)
-       (or (and
-            (= emacs-major-version 19)
-            (>= emacs-minor-version 14))
-           (= emacs-major-version 20))
-       (fboundp 'load-options-file))
-  (load-options-file "/toasters/toaster5/users/ryand/.xemacs-options")))
-;; ============================
-;; End of Options Menu Settings
-
 (custom-set-variables
- '(bar-cursor 2)
+ '(backup-by-copying-when-linked t)
+ '(paren-mode (quote paren) nil (paren))
+ '(c-echo-syntactic-information-p t)
+ '(browse-url-browser-function (quote browse-url-w3))
+ '(inhibit-startup-message t t)
+ '(cperl-help t)
+ '(bar-cursor (quote (quote other)))
+ '(jde-jdk-doc-url "file:///usr/share/doclib/java/index.html")
+ '(eldoc-mode t)
+ '(toolbar-visible-p nil)
  '(time-stamp-format "%02y-%02m-%02d %02H:%02M:%02S %u")
+ '(line-number-mode t)
+ '(calendar-time-display-form (quote (24-hours ":" minutes (if time-zone " (") time-zone (if time-zone ")"))))
+ '(cperl-hairy t)
  '(user-mail-address "ryand@amazon.com" t)
  '(query-user-mail-address nil))
-(custom-set-faces)
+(custom-set-faces
+ '(default ((t (:foreground "black" :background "white"))) t))
