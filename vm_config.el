ac94ce473216c7249cc46dd9172fd92452c26545
Ryan Davis <ryand@zenspider.com>
1999-06-14 18:09:01 -0800

    Changed include prefix to be wrap friendly. Removed print command switches. Removed auto-folder-alist because I don't use it anymore. Added qa mailbox
    [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 117]

--- vm_config.el
+++ vm_config.el
@@ -11,6 +11,8 @@
  vm-auto-get-new-mail 10
  vm-mail-check-interval 30
  
+ vm-included-text-prefix "> "
+
  vm-delete-after-saving t
  vm-delete-after-archiving t
  
@@ -27,39 +29,39 @@
  vm-reply-subject-prefix "Re: "
  vm-subject-ignored-prefix "^\\(re\\(\\[[0-9]+\\]\\)?: *\\)+"
 
- vm-print-command-switches lpr-switches
+;; vm-print-command-switches lpr-switches
 
  ;; Ignore case
  vm-auto-folder-case-fold-search t
 
  ;; My list of folders to automatically save to
- vm-auto-folder-alist '(
-			("From:"
-			 ("ryand" . "ryand")
-			 ("bosik" . "kathy")
-			 ("pwilk" . "phil")
-			 ("alanh" . "alan")
-			 ("bill@" . "bill")
-			 ("snoc"  . "snoc")
-			 )
-			(
-			 "Subject:"
-			 ("checkin" . "commit")
-			 ("commit" . "commit")
-			 )
-			(
-			 "To:"
-			 ("software@" . "software")
-			 ("qa@" . "software")
-			 ("downtown" . "downtown")
-			 )
-			(
-			 "CC:"
-			 ("software@" . "software")
-			 ("qa@" . "software")
-			 ("downtown" . "downtown")
-			 )
-			)
+; vm-auto-folder-alist '(
+;			("From:"
+;			 ("ryand" . "ryand")
+;			 ("bosik" . "kathy")
+;			 ("pwilk" . "phil")
+;			 ("alanh" . "alan")
+;			 ("bill@" . "bill")
+;			 ("snoc"  . "snoc")
+;			 )
+;			(
+;			 "Subject:"
+;			 ("checkin" . "commit")
+;			 ("commit" . "commit")
+;			 )
+;			(
+;			 "To:"
+;			 ("software@" . "software")
+;			 ("qa@" . "qa")
+;			 ("downtown" . "downtown")
+;			 )
+;			(
+;			 "CC:"
+;			 ("software@" . "software")
+;			 ("qa@" . "software")
+;			 ("downtown" . "downtown")
+;			 )
+;			)
  vm-spool-files (list 
  		 (list "~/Mail/INBOX" "~/Mail/spool/INBOX" "~/Mail/INBOX.CRASH")
  		 (list "~/Mail/bill" "~/Mail/spool/bill" "~/Mail/bill.CRASH")
@@ -70,6 +72,7 @@
  		 (list "~/Mail/kathy" "~/Mail/spool/kathy" "~/Mail/kathy.CRASH")
  		 (list "~/Mail/ryand" "~/Mail/spool/ryand" "~/Mail/ryand.CRASH")
  		 (list "~/Mail/software" "~/Mail/spool/software" "~/Mail/software.CRASH")
+ 		 (list "~/Mail/qa" "~/Mail/spool/qa" "~/Mail/qa.CRASH")
  		 (list "~/Mail/ssc" "~/Mail/spool/ssc" "~/Mail/ssc.CRASH")
  		 (list "~/Mail/test" "~/Mail/spool/test" "~/Mail/test.CRASH")
  		 (list "~/Mail/zss" "~/Mail/spool/zss" "~/Mail/zss.CRASH")
@@ -83,4 +86,4 @@
  ;;			     (text "another-regexp")))))
  )
 
-;;(bbdb-insinuate-vm)
+; (bbdb-insinuate-vm)
