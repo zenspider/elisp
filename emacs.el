0f40dc182baeb507af0c7cf2b1f1b71af702c4b2
Ryan Davis <ryand@zenspider.com>
2000-12-20 18:17:54 -0800

    Added third-party paths to elisp dir. Tweaked some stuff for jde. (WHY HERE?)
    [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 543]

--- emacs.el
+++ emacs.el
@@ -1,8 +1,15 @@
 ;;; Add my elisp directory to pathing
 (pushnew (expand-file-name "~/Bin/elisp/")
 	 load-path :test 'string=)
+(pushnew (expand-file-name "~/Bin/elisp/third-party/")
+	 load-path :test 'string=)
+(pushnew (expand-file-name "~/Bin/elisp/third-party/semantic")
+	 load-path :test 'string=)
+(pushnew (expand-file-name "~/Bin/elisp/third-party/jde/lisp")
+	 load-path :test 'string=)
 
-(byte-recompile-directory (expand-file-name "~/Bin/elisp") 0)
+(byte-recompile-directory (expand-file-name "~/Bin/elisp") 0 t)
+; (byte-recompile-directory (expand-file-name "~/Bin/elisp") 0)
 
 (require 'vc-hooks)
 ;(require 'project)
@@ -19,8 +26,10 @@
  '(gutter-buffers-tab-visible-p nil)
  '(backup-by-copying-when-linked t)
  '(paren-mode (quote paren) nil (paren))
- '(delete-key-deletes-forward nil)
  '(gutter-visible-p nil)
+ '(jde-make-program "gmake")
+ '(jde-auto-parse-buffer-interval 60)
+ '(delete-key-deletes-forward nil)
  '(lazy-shot-mode t nil (lazy-shot))
  '(c-echo-syntactic-information-p t)
  '(browse-url-browser-function (quote browse-url-w3))
@@ -29,6 +38,7 @@
  '(bar-cursor (quote (quote other)))
  '(jde-jdk-doc-url "file:///usr/share/doclib/java/index.html")
  '(eldoc-mode t)
+ '(jde-gen-get-set-var-template (quote ("(end-of-line) '&" "(P \"Variable type: \" type) \" \"" "(P \"Variable name: \" name) \";\" '>'n '>'n" "\"/**\" '>'n" "\"* Get the value of \" (s name) \".\" '>'n" "\"* @return value of \" (s name) \".\" '>'n" "\"**/\" '>'n '>'n" " \"public \" (s type)" "(if (string= \"boolean\" (jde-gen-lookup-named 'type) ) " "\" is\" " "\" \" ) " "(s name)" "\"() \"" "(if jde-gen-k&r " "()" "'>'n)" "\"{\" '>'n" "\"return this.\" (s name) \";\" '>'n \"}\"" "'>'n '>'n" "\"/**\" '>'n" "\"* Set the value of \" (s name) \".\" '>'n" "\"* @param \" (s name) \" Value to assign to \" (s name) \".\" '>'n" "\"**/\" '>'n '>'n" "\"public void \" (s name)" "\"(\" (s type) \" \" (s name) \") \" " "(if jde-gen-k&r " "()" "'>'n)" "\"{\" '>'n" "'>'n \"this.\" (s name) \" = \" (s name) \";\" '>'n \"}\" '>'n'>")))
  '(shell-multiple-shells t)
  '(toolbar-visible-p nil)
  '(jde-build-use-make t)
@@ -52,6 +62,6 @@
             (>= emacs-minor-version 14))
            (= emacs-major-version 20))
        (fboundp 'load-options-file))
-  (load-options-file "/toasters/toaster5/users/ryand/.xemacs-options")))
+  (load-options-file "/home/ryand/.xemacs-options")))
 ;; ============================
 ;; End of Options Menu Settings
