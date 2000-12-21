5af7b305f23af408df1732d39dab26b57f3e8533
Ryan Davis <ryand@zenspider.com>
2000-12-20 18:18:00 -0800

    Added some mailboxes. Added iso-8859.
    [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 546]

--- vm_config.el
+++ vm_config.el
@@ -3,6 +3,7 @@
 (add-to-list 'vm-mime-default-face-charsets "Windows-1251")
 (add-to-list 'vm-mime-default-face-charsets "Windows-1252")
 (add-to-list 'vm-mime-default-face-charsets "Windows-1257")
+(add-to-list 'vm-mime-default-face-charsets "iso-8859-9")
 
 (setq 
 
@@ -43,23 +44,29 @@
  ;; Spool setup
  vm-spool-files (list 
  		 (list "~/Mail/INBOX" "~/Mail/spool/INBOX" "~/Mail/INBOX.CRASH")
+ 		 (list "~/Mail/GrayBox" "~/Mail/spool/GrayBox" "~/Mail/GrayBox.CRASH")
+ 		 (list "~/Mail/BlackBox" "~/Mail/spool/BlackBox" "~/Mail/BlackBox.CRASH")
  		 (list "~/Mail/antlr" "~/Mail/spool/antlr" "~/Mail/antlr.CRASH")
  		 (list "~/Mail/bill" "~/Mail/spool/bill" "~/Mail/bill.CRASH")
  		 (list "~/Mail/build" "~/Mail/spool/build" "~/Mail/build.CRASH")
  		 (list "~/Mail/commit" "~/Mail/spool/commit" "~/Mail/commit.CRASH")
  		 (list "~/Mail/commit-main" "~/Mail/spool/commit-main" "~/Mail/commit-main.CRASH")
+ 		 (list "~/Mail/cvs" "~/Mail/spool/cvs" "~/Mail/cvs.CRASH")
  		 (list "~/Mail/downtown" "~/Mail/spool/downtown" "~/Mail/downtown.CRASH")
  		 (list "~/Mail/frank" "~/Mail/spool/frank" "~/Mail/frank.CRASH")
  		 (list "~/Mail/crontab" "~/Mail/spool/crontab" "~/Mail/crontab.CRASH")
  		 (list "~/Mail/kathy" "~/Mail/spool/kathy" "~/Mail/kathy.CRASH")
  		 (list "~/Mail/perf" "~/Mail/spool/perf" "~/Mail/perf.CRASH")
- 		 (list "~/Mail/poker" "~/Mail/spool/poker" "~/Mail/poker.CRASH")
+ 		 (list "~/Mail/progress" "~/Mail/spool/progress" "~/Mail/progress.CRASH")
+ 		 (list "~/Mail/junit" "~/Mail/spool/junit" "~/Mail/junit.CRASH")
+ 		 (list "~/Mail/obidos" "~/Mail/spool/obidos" "~/Mail/obidos.CRASH")
  		 (list "~/Mail/other" "~/Mail/spool/other" "~/Mail/other.CRASH")
  		 (list "~/Mail/qa" "~/Mail/spool/qa" "~/Mail/qa.CRASH")
  		 (list "~/Mail/ryand" "~/Mail/spool/ryand" "~/Mail/ryand.CRASH")
  		 (list "~/Mail/software" "~/Mail/spool/software" "~/Mail/software.CRASH")
  		 (list "~/Mail/ssc" "~/Mail/spool/ssc" "~/Mail/ssc.CRASH")
  		 (list "~/Mail/test" "~/Mail/spool/test" "~/Mail/test.CRASH")
+ 		 (list "~/Mail/backup" "~/Mail/spool/backup" "~/Mail/backup.CRASH")
  		 (list "~/Mail/zss" "~/Mail/spool/zss" "~/Mail/zss.CRASH")
  		 (list "~/Mail/xmas" "~/Mail/spool/xmas" "~/Mail/xmas.CRASH")
 		 )
