1a7185d53d5b272930f29cf9792ecac0ccd3df88
Ryan Davis <ryand@zenspider.com>
2001-08-07 08:46:08 -0800

    src/Makefile:   Made recursive make breakable. Satori/src/GNUMakefile:   Connected to Jam. ZenWeb/TestZenWeb.rb:   exits w/ test results now. pkg_clean/GNUMakefile:   Calls bsd make. Bin/Config/xsession:   Turns off caps lock. Opens xterm. Bin/elisp/setup-keys.el:   Minor cleanup.
    [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 823]

--- setup-keys.el
+++ setup-keys.el
@@ -5,10 +5,10 @@
 ; (global-unset-key "\C-x\C-c") ;; Annoying behavior; save-buffers-kill-emacs
 (global-unset-key 'insert) ; Annoying behavior
 
-(global-set-key 'f1  'undo)                         ;; undo
-(global-set-key 'f2  'x-kill-primary-selection)     ;; cut
-(global-set-key 'f3  'x-copy-primary-selection)     ;; copy
-(global-set-key 'f4  'x-yank-clipboard-selection)   ;; paste
+; (global-set-key 'f1  'undo)                         ;; undo
+; (global-set-key 'f2  'x-kill-primary-selection)     ;; cut
+; (global-set-key 'f3  'x-copy-primary-selection)     ;; copy
+; (global-set-key 'f4  'x-yank-clipboard-selection)   ;; paste
 (global-set-key 'f5  'kill-buffer-and-close-window) ;; close
 (global-set-key 'f6  'windoze-sucks)
 (global-set-key 'f6  'clean-whitespace)             ;; clean (setup-aliases.el)
