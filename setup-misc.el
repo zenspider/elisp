a1487a124eda07291ad4106f8eaca314a63d524e
Ryan Davis <ryand@zenspider.com>
2001-08-22 09:31:01 -0800

    Turn on wrapping on vertical split panes
    [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 827]

--- setup-misc.el
+++ setup-misc.el
@@ -6,12 +6,9 @@
       indent-tabs-mode t
       efs-default-account "ryand"
       lpr-switches '("-Pcow")
-      time-stamp-format "%04y-%02m-%02d %02H:%02M:%02S %u")
-
-;; (put 'upcase-region 'disabled nil)
+      time-stamp-format "%04y-%02m-%02d %02H:%02M:%02S %u"
+      truncate-partial-width-windows nil
+      )
 
 (set-glyph-image modeline-pointer-glyph "leftbutton")
-; (gnuserv-start)
 
-;(setq Info-directory-list
-;      (append Info-directory-list '("/usr/obj/usr/src/gnu/usr.bin/cvs/doc/")))
