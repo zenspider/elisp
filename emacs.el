960901aa44bcaff11b4d5465a40989f94750ccdd
Ryan Davis <ryand@zenspider.com>
2002-04-26 17:54:29 -0800

    Tweaks for stability
    [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 961]

--- emacs.el
+++ emacs.el
@@ -1,93 +1,99 @@
 ; Stupid compatibility checking
+
 (unless (featurep 'xemacs)
 
   (message "Have I ever mentioned that GNU emacs sucks?")
+
   (defun add-to-list (list-var element)
     "Stolen from xemac's subr.el"
     (or (member element (symbol-value list-var))
 	(set list-var (cons element (symbol-value list-var))))) 
+
   (defalias 'mapc 'mapcar)
+
   (load-library "cl")
-  (eval "(setq apropos-do-all t)") ; FIX not sure this works
+
+  (setq apropos-do-all t)
+
   (defalias 'Info-directory-list 'Info-default-directory-list)
   )
 
-; REFACTOR: generalize both lambdas into pushnew-string=(list, ...)
 (if (featurep 'xemacs)
+
+    (message "you are running xemacs... good")
+
     ;; Extend Info directories
-    (map nil (function (lambda (path)
-			 (pushnew path Info-directory-list :test 'string=)))
-	 (list 
-	  "/usr/share/info"
-	  (expand-file-name "~/Bin/elisp/third-party/tramp")
-	  ))
-  )
+    (mapc '(lambda (x)
+	     (pushnew x Info-directory-list :test 'string=))
+	  (list 
+	   "/usr/share/info"
+	   (expand-file-name "~/Bin/elisp/third-party/tramp")
+	   ))
+)
 
-; FIX: function should use something other that pushnew for GNU emacs compat.
-(map nil (function (lambda (path)
-		     (pushnew (expand-file-name path)
-			      load-path :test 'string=)))
-     (list
-      "/usr/local/lib/xemacs/site-lisp"
-      "~/Bin/elisp/"
-      "~/Bin/elisp/third-party/"
-      "~/Bin/elisp/third-party/tramp"
-      "~/Bin/elisp/third-party/semantic"
-      "~/Bin/elisp/third-party/jde/lisp"
-      ))
+;;; Add my elisp directory to pathing
 
-;; Extend Info directories
-(setq Info-directory-list (cons "/usr/share/info" Info-directory-list))
+(mapc '(lambda (path)
+	 (pushnew (expand-file-name path) load-path :test 'string=))
+      (list 
+       "/usr/local/lib/xemacs/site-lisp/"
+       "~/Bin/elisp/"
+       "~/Bin/elisp/third-party/"
+       "~/Bin/elisp/third-party/tramp"
+       "~/Bin/elisp/third-party/semantic"
+       "~/Bin/elisp/third-party/jde/lisp"
+       ))
 
-; in the case of loading from a tty, I'm assuming that we are looking
-; for as much speed as possible as our lifetime is probably
-; short-lived.
+;(byte-recompile-directory (expand-file-name "~/Bin/elisp") 0 t)
 
-(cond 
- ((eq 'tty (device-type))
-  (setq delete-key-deletes-forward nil)
-  (message "Console setup loaded")
-  )
- ((eq 'x (device-type))
+(require 'vc-hooks)
 
-; Compile everything in elisp, but not recursively, as that seems to
-; crash a lot.
-  (byte-recompile-directory (expand-file-name "~/Bin/elisp") 0 t)
-  
-  (custom-set-variables
-; '(bar-cursor (quote (quote other)))
-; '(browse-url-browser-function (quote browse-url-w3))
-   '(c-echo-syntactic-information-p t)
-   '(column-number-mode t)
-; '(cperl-help t)
-   '(delete-key-deletes-forward nil)
-   '(eldoc-mode t)
-   '(font-lock-mode t nil (font-lock))
-; '(gutter-buffers-tab-visible-p nil)
-; '(gutter-visible-p nil)
-   '(inhibit-startup-message t t)
-; '(jde-auto-parse-buffer-interval 60)
-; '(jde-make-program "gmake")
-; '(lazy-lock-mode t nil (lazy-lock))
-   '(line-number-mode t)
-   '(paren-mode (quote paren) nil (paren))
-   '(query-user-mail-address nil)
-; '(revert-without-query (quote ("\\.html")))
-   '(shell-multiple-shells t)
-; '(sql-postgres-options (quote ("-P pager=off")))
-; '(toolbar-visible-p nil)
-   '(user-mail-address (concat "ryand@" (getenv "DOMAIN")))
-   )
-  
-  (custom-set-faces);
-  
-  (load "setup-aliases")
-  (load "setup-keys")
-  (load "setup-mail-and-news")
-  (load "setup-misc")
-  (load "setup-modes")
-  (require 'bs)
-  (require 'vc-hooks)
-  )
- (t (message "Unknown device-type, no extra initialization"))
- )
+;;; Load My Stuff
+(load "setup-aliases")
+(load "setup-keys")
+(load "setup-mail-and-news")
+(load "setup-misc")
+(load "setup-modes")
+(load "bs")
+
+(custom-set-variables
+ '(gutter-buffers-tab-visible-p nil)
+ '(paren-mode (quote paren) nil (paren))
+ '(gutter-visible-p nil)
+ '(jde-make-program "gmake")
+ '(jde-auto-parse-buffer-interval 60)
+ '(delete-key-deletes-forward nil)
+ '(column-number-mode t)
+ '(lazy-lock-mode t nil (lazy-lock))
+ '(c-echo-syntactic-information-p t)
+ '(sql-postgres-options (quote ("-P pager=off")))
+ '(browse-url-browser-function (quote browse-url-w3))
+ '(inhibit-startup-message t t)
+ '(cperl-help t)
+ '(bar-cursor (quote (quote other)))
+ '(jde-jdk-doc-url "file:///usr/share/doclib/java/index.html")
+ '(eldoc-mode t)
+ '(jde-gen-get-set-var-template (quote ("(end-of-line) '&" "(P \"Variable type: \" type) \" \"" "(P \"Variable name: \" name) \" ;\" '>'n '>'n" "\"/**\" '>'n" "\"* Get the value of \" (s name) \".\" '>'n" "\"* @return value of \" (s name) \".\" '>'n" "\"**/\" '>'n '>'n" " \"public \" (s type)" "(if (string= \"boolean\" (jde-gen-lookup-named 'type) ) " "\" is\" " "\" \" ) " "(s name)" "\"() \"" "(if jde-gen-k&r " "()" "'>'n)" "\"{\" '>'n" "\"return this.\" (s name) \";\" '>'n \"}\"" "'>'n '>'n" "\"/**\" '>'n" "\"* Set the value of \" (s name) \".\" '>'n" "\"* @param \" (s name) \" Value to assign to \" (s name) \".\" '>'n" "\"**/\" '>'n '>'n" "\"public void \" (s name)" "\"(\" (s type) \" \" (s name) \") \" " "(if jde-gen-k&r " "()" "'>'n)" "\"{\" '>'n" "'>'n \"this.\" (s name) \" = \" (s name) \";\" '>'n \"}\" '>'n'>")))
+ '(shell-multiple-shells t)
+ '(toolbar-visible-p nil)
+ '(line-number-mode t)
+ '(revert-without-query (quote ("\\.html")))
+ '(query-user-mail-address nil)
+ '(font-lock-mode t nil (font-lock))
+ '(user-mail-address (concat "ryand@" (getenv "DOMAIN"))))
+
+(custom-set-faces)
+
+;; Options Menu Settings
+;; =====================
+(cond
+ ((and (string-match "XEmacs" emacs-version)
+       (boundp 'emacs-major-version)
+       (or (and
+            (= emacs-major-version 19)
+            (>= emacs-minor-version 14))
+           (= emacs-major-version 20))
+       (fboundp 'load-options-file))
+  (load-options-file "/home/ryand/.xemacs-options")))
+;; ============================
+;; End of Options Menu Settings
