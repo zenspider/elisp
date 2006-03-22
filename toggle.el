26bb79dd060fb3823cfd7155f3e90e958576fc27
Ryan Davis <ryand@zenspider.com>
2006-03-22 01:17:28 -0800

    YAY! I skipped step 2 and flat out made it search a list of regex!
    [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 2402]

--- toggle.el
+++ toggle.el
@@ -1,31 +1,36 @@
 ;; toggle.el
 ;
-; DONE:
 ; blah.rb <-> test_blah.rb
-;
-; TODO:
 ; app/model/blah.rb <-> test/unit/blah_test.rb
 ; lib/blah.rb <-> test/test_blah.rb
 
-(defun impl-to-test (bfn)
-  (let ((dir-name (file-name-directory bfn))
-	(file-name (file-name-nondirectory bfn)))
-    (concat dir-name "test_" file-name)))
+(defvar toggle-mappings
+  (list
+   (cons "app/model/\\(.*\\).rb" "test/unit/\\1_test.rb")
+   (cons "test/unit/\\(.*\\)_test.rb" "app/model/\\1.rb")
+   (cons "lib/\\(.*\\).rb" "test/test_\\1.rb")
+   (cons "test/test_\\(.*\\).rb" "lib/\\1.rb")
+   (cons "test_\\(.*\\).rb" "\\1.rb")
+   (cons "\\([^/]+\\).rb" "test_\\1.rb")))
 
-(defun test-to-impl (bfn)
-  (let ((dir-name (file-name-directory bfn))
-	(file-name (substring (file-name-nondirectory bfn) 5)))
-    (concat dir-name file-name)))
-
-(defun filename-test-p (bfn)
-  (string-match "test_" bfn))
-
-(defun toggle-filename (bfn)
-  (cond ((filename-test-p bfn) (test-to-impl bfn))
-	('t (impl-to-test bfn))))
+; this was stolen from toggle-source.el, I don't think it is very "lisp-like"
+(defun toggle-filename (filename)
+  (let ((list nil)
+	(entry nil)
+	(found nil))
+    (setq list toggle-mappings)
+    (while (and list (not found) filename)
+      (setq entry (car list))
+      (setq list  (cdr list))
+      (if (string-match (car entry) filename)
+	  (setq found (replace-match (cdr entry) nil nil filename))))
+    found))
 
 (defun toggle-buffer ()
   (interactive)
-  (find-file (toggle-filename (buffer-file-name))))
+  (let ((new-name (toggle-filename (buffer-file-name))))
+    (if new-name
+	(find-file new-name)
+      (message (concat "Match not found for " (buffer-file-name))))))
 
 (provide 'toggle)
