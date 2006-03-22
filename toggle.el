de476fca9e8c0def9e99852b66403888e92bfee8
Ryan Davis <ryand@zenspider.com>
2006-03-22 01:18:31 -0800

    Cleaned up with a proper lisp-like implementation. Pretty now!
    [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 2403]

--- toggle.el
+++ toggle.el
@@ -13,18 +13,11 @@
    (cons "test_\\(.*\\).rb" "\\1.rb")
    (cons "\\([^/]+\\).rb" "test_\\1.rb")))
 
-; this was stolen from toggle-source.el, I don't think it is very "lisp-like"
-(defun toggle-filename (filename)
-  (let ((list nil)
-	(entry nil)
-	(found nil))
-    (setq list toggle-mappings)
-    (while (and list (not found) filename)
-      (setq entry (car list))
-      (setq list  (cdr list))
-      (if (string-match (car entry) filename)
-	  (setq found (replace-match (cdr entry) nil nil filename))))
-    found))
+(defun toggle-filename (path paths)
+  (cond ((null paths) '())
+	((string-match (caar paths) path)
+	 (replace-match (cdar paths) nil nil path))
+	('t (toggle-filename path (cdr paths)))))
 
 (defun toggle-buffer ()
   (interactive)
