223efbba9cffdeb9946a705e23222b08cd828f39
Ryan Davis <ryand@zenspider.com>
2007-01-28 12:43:25 -0800

    Sorted defuns and other minor cleanup. Added locate command to do filename searches only.
    [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 2978]

--- setup-aliases.el
+++ setup-aliases.el
@@ -2,19 +2,9 @@
 ;; Aliases:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;; Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph
-;;; Takes a multi-line paragraph and makes it into a single line of text.
-(defun unfill-paragraph ()
-  (interactive)
-  (let ((fill-column (point-max)))
-    (fill-paragraph nil)))
-
-(defun current-frame ()
-  (car (car (last (cdr (current-frame-configuration))))))
-
 (defun arrange-frame (w h &optional nosplit)
   "Rearrange the current frame to a custom width and height and split unless prefix."
-  (let ((frame (current-frame)))
+  (let ((frame (selected-frame)))
     (when (equal 'mac (framep frame))
       (delete-other-windows)
       (set-frame-position frame 5 25)
@@ -22,17 +12,38 @@
       (if (not nosplit)
           (split-window-horizontally)))))
 
-(defun small (&optional split)
-  "Create a small window suitable for coding on anything."
-  (interactive "P")
-  (my-set-mac-font "bitstream vera sans mono" 12)
-  (arrange-frame 80 45 (not split)))
+(defun clean-whitespace ()
+  (interactive)
+  (save-excursion
+    (save-restriction
+      (save-match-data
+    (progn
+      (delete-trailing-whitespace)
+      (goto-char (point-min))
+      (while (re-search-forward "^[ \t]+" nil t)
+        (delete-region (match-beginning 0) (match-end 0)))
+      (goto-char (point-min))
+      (while (re-search-forward " +" nil t)
+        (delete-region (+ 1 (match-beginning 0)) (match-end 0)))
+      (goto-char (point-min))
+      (while (re-search-forward "\n\n+" nil t)
+        (delete-region (+ 2 (match-beginning 0)) (match-end 0)))
+      (goto-char (point-min))
+      (while (looking-at "\n")
+        (delete-char 1))
+      (goto-char (- (point-max) 1))
+      (while (looking-at "\n")
+        (delete-char 1)))))))
 
-(defun medium (&optional nosplit)
-  "Create a large window suitable for coding on a macbook."
-  (interactive "P")
-  (my-set-mac-font "bitstream vera sans mono" 12)
-  (arrange-frame 170 45 nosplit))
+(defun forward-line-6 ()
+  (interactive)
+  (forward-line 6))
+
+(defun grep-current-word ()
+  "Grep for the current word"
+  (interactive)
+  (let ((current-prefix-arg t))
+    (call-interactively 'grep)))
 
 (defun huge (&optional nosplit)
   "Create a really large window suitable for coding on a 20 inch cinema display."
@@ -40,39 +51,20 @@
   (my-set-mac-font "bitstream vera sans mono" 12)
   (arrange-frame 199 60 nosplit))
 
-(defun presentation ()
-  "Create a giant font window suitable for doing live demos."
+(defun insert-buffer-name()
+  "Insert the value of buffer-name"
   (interactive)
-  (arrange-frame 85 25 t)
-  (my-set-mac-font "bitstream vera sans mono" 24))
+  (progn
+    (insert (buffer-name))))
 
-(defun peepcode ()
-  "Create a small font window suitable for doing live demos in 800x600."
-  (interactive)
-  (arrange-frame 85 34 t)
-  (my-set-mac-font "bitstream vera sans mono" 14))
+(defun locate-make-mdfind-command-line (search-string)
+  (list "mdfind" (concat "kMDItemDisplayName=*" search-string "*")))
 
-(defun record-current-window ()
-  (delete-other-windows)
-  (insert (pp `(arrange-frame ,(window-width) ,(+ 1 (window-height)) t))))
-
-(defun my-recompile-init ()
-  (interactive)
-  (byte-recompile-directory (expand-file-name "~/Bin/elisp") 0))
-
-(defun my-generate-new-buffer-name (name)
-  "Find a new buffer name not currently used in the form of <name>-<N> where N starts at 1"
-  (let* ((count 1)
-         (buffer-name (format "%s-%d" name count)))
-    (while (get-buffer buffer-name)
-      (set 'buffer-name (format "%s-%d" name count))
-      (set 'count (+ count 1)))
-    buffer-name))
-
-(defun myshell ()
-  "Create a shell buffer that is properly named (shell-<N>)"
-  (interactive)
-  (shell (my-generate-new-buffer-name "shell")))
+(defun medium (&optional nosplit)
+  "Create a large window suitable for coding on a macbook."
+  (interactive "P")
+  (my-set-mac-font "bitstream vera sans mono" 12)
+  (arrange-frame 170 45 nosplit))
 
 (defun modify-tabs (length enabled)
   (progn
@@ -83,104 +75,7 @@
           (tabify (mark) (point))
           (untabify (mark) (point))))))
 
-(defun mytabs()
-  "Set tabbing to spaces at 2 col tabstops."
-  (interactive)
-  (modify-tabs 2 nil))
-
-(defun mytabs4()
-  "Set tabbing to spaces at 4 col tabstops."
-  (interactive)
-  (modify-tabs 4 nil))
-
-(defun mytabs-hard ()
-  "Set tabbing to real tabs but viewed at 4 col tabstops."
-  (interactive)
-  (modify-tabs 4 t))
-
-(defun toggle-split ()
-  "Toggle vertical/horizontal window split."
-  (interactive)
-  (if (one-window-p)
-      (error "Frame doesn't have two windows")
-    (let* ((cw (selected-window))
-           (nw (next-window cw))
-           (wf (window-frame cw))
-           ;;(cb (buffer-name (window-buffer cw)))
-           (nb (buffer-name (window-buffer nw)))
-           (sv (if (eq (window-width cw) (frame-width wf))
-                   t nil)))
-      (delete-window nw)
-      (split-window cw nil sv)
-      (switch-to-buffer-other-window nb)
-      )))
-
-(defun swap-buffers ()
-  "Swap the current 2 buffers in their windows"
-  (interactive)
-  (if (one-window-p)
-      (error "Frame doesn't have two windows")
-    (let* ((cw (selected-window))
-           (nw (next-window cw))
-           (cb (buffer-name (window-buffer cw)))
-           (nb (buffer-name (window-buffer nw))))
-      (switch-to-buffer nb)
-      (select-window nw)
-      (switch-to-buffer cb))))
-
-(defun insert-buffer-name()
-  "Insert the value of buffer-name"
-  (interactive)
-  (progn
-    (insert (buffer-name))))
-
-(defun reload-safari ()
-  (interactive)
-  (shell-command "printf 'tell application \"System Events\"\nclick button \"Stop\" of first window of process \"Safari\"\nend tell' | osascript" nil nil))
-
-(defun clean-whitespace ()
-  (interactive)
-  (save-excursion
-    (save-restriction
-      (save-match-data
-	(progn
-	  (delete-trailing-whitespace)
-	  (goto-char (point-min))
-	  (while (re-search-forward "^[ \t]+" nil t)
-	    (delete-region (match-beginning 0) (match-end 0)))
-	  (goto-char (point-min))
-	  (while (re-search-forward " +" nil t)
-	    (delete-region (+ 1 (match-beginning 0)) (match-end 0)))
-	  (goto-char (point-min))
-	  (while (re-search-forward "\n\n+" nil t)
-	    (delete-region (+ 2 (match-beginning 0)) (match-end 0)))
-	  (goto-char (point-min))
-	  (while (looking-at "\n")
-	    (delete-char 1))
-	  (goto-char (- (point-max) 1))
-	  (while (looking-at "\n")
-	    (delete-char 1)))))))
-
-(defun forward-line-6 ()
-  (interactive)
-  (forward-line 6))
-
-(defun previous-line-6 ()
-  (interactive)
-  (previous-line 6))
-
-(defun grep-current-word ()
-  "Grep for the current word"
-  (interactive)
-  (let ((current-prefix-arg t))
-    (call-interactively 'grep)))
-
-(defun server-stop ()
-  "Stop the server"
-  (interactive)
-  (server-start t))
-
-(defun fc-eval-and-replace ()
+(defun my-eval-and-replace ()
   "Replace the preceding sexp with its value."
   (interactive)
   (backward-kill-sexp)
@@ -189,6 +84,23 @@
              (current-buffer))
     (error (message "Invalid expression")
            (insert (current-kill 0)))))
+
+(defun my-generate-new-buffer-name (name)
+  "Find a new buffer name not currently used in the form of <name>-<N> where N starts at 1"
+  (let* ((count 1)
+         (buffer-name (format "%s-%d" name count)))
+    (while (get-buffer buffer-name)
+      (set 'buffer-name (format "%s-%d" name count))
+      (set 'count (+ count 1)))
+    buffer-name))
+
+(defun my-recompile-init ()
+  (interactive)
+  (byte-recompile-directory (expand-file-name "~/Bin/elisp") 0))
+
+(defun my-record-current-window ()
+  (delete-other-windows)
+  (insert (pp `(arrange-frame ,(window-width) ,(+ 1 (window-height)) t))))
 
 (defun my-selective-display (column)
   "Rotate folding the buffer at no, 2, 4, and 6 columns."
@@ -214,3 +126,89 @@
 
 ;; to get the current font:
 ; (frame-parameter nil 'font)
+
+(defun myshell ()
+  "Create a shell buffer that is properly named (shell-<N>)"
+  (interactive)
+  (shell (my-generate-new-buffer-name "shell")))
+
+(defun mytabs()
+  "Set tabbing to spaces at 2 col tabstops."
+  (interactive)
+  (modify-tabs 2 nil))
+
+(defun mytabs-hard ()
+  "Set tabbing to real tabs but viewed at 4 col tabstops."
+  (interactive)
+  (modify-tabs 4 t))
+
+(defun mytabs4()
+  "Set tabbing to spaces at 4 col tabstops."
+  (interactive)
+  (modify-tabs 4 nil))
+
+(defun peepcode ()
+  "Create a small font window suitable for doing live demos in 800x600."
+  (interactive)
+  (arrange-frame 85 34 t)
+  (my-set-mac-font "bitstream vera sans mono" 14))
+
+(defun presentation ()
+  "Create a giant font window suitable for doing live demos."
+  (interactive)
+  (arrange-frame 85 25 t)
+  (my-set-mac-font "bitstream vera sans mono" 24))
+
+(defun previous-line-6 ()
+  (interactive)
+  (previous-line 6))
+
+(defun reload-safari ()
+  (interactive)
+  (shell-command "printf 'tell application \"System Events\"\nclick button \"Stop\" of first window of process \"Safari\"\nend tell' | osascript" nil nil))
+
+(defun server-stop ()
+  "Stop the server"
+  (interactive)
+  (server-start t))
+
+(defun small (&optional split)
+  "Create a small window suitable for coding on anything."
+  (interactive "P")
+  (my-set-mac-font "bitstream vera sans mono" 12)
+  (arrange-frame 80 45 (not split)))
+
+(defun swap-buffers ()
+  "Swap the current 2 buffers in their windows"
+  (interactive)
+  (if (one-window-p)
+      (error "Frame doesn't have two windows")
+    (let* ((cw (selected-window))
+           (nw (next-window cw))
+           (cb (buffer-name (window-buffer cw)))
+           (nb (buffer-name (window-buffer nw))))
+      (switch-to-buffer nb)
+      (select-window nw)
+      (switch-to-buffer cb))))
+
+(defun toggle-split ()
+  "Toggle vertical/horizontal window split."
+  (interactive)
+  (if (one-window-p)
+      (error "Frame doesn't have two windows")
+    (let* ((cw (selected-window))
+           (nw (next-window cw))
+           (wf (window-frame cw))
+           ;;(cb (buffer-name (window-buffer cw)))
+           (nb (buffer-name (window-buffer nw)))
+           (sv (if (eq (window-width cw) (frame-width wf))
+                   t nil)))
+      (delete-window nw)
+      (split-window cw nil sv)
+      (switch-to-buffer-other-window nb)
+      )))
+
+(defun unfill-paragraph ()
+  (interactive)
+  (let ((fill-column (point-max)))
+    (fill-paragraph nil)))
