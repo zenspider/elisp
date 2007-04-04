dff201077d9a559c8cc88ae11dc45097b1ce9c41
Ryan Davis <ryand@zenspider.com>
2007-04-03 19:19:47 -0800

    Added a fair amount of polish. Almost ready for release.
    [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 3090]

--- autotest.el
+++ autotest.el
@@ -1,12 +1,18 @@
-;; autotest.el
-;
-; sets up an autotest buffer and provides conveniencte methods 
+;; autotest.el - by Ryan Davis - ryan-ruby@zenspider.com
+;;
+;; Sets up an autotest buffer and provides conveniencte methods.
+;;
+;; History:
+;; 1.0 beta 2 - 2007-04-03 - added autotest plugin / communication support
+;; 1.0 beta 1 - 2007-03-06 - initial release
 
 (require 'shell)
 
 (defun autotest ()
+  "Fire up an instance of autotest in its own buffer with shell bindings and compile-mode highlighting and linking."
   (interactive)
-  (let ((buffer (shell "autotest")))
+  (let ((buffer (shell "*autotest*")))
+
     (define-key shell-mode-map "\C-c\C-a" 'autotest-switch)
 
     (set (make-local-variable 'comint-output-filter-functions)
@@ -19,32 +25,43 @@
          '(
            ("^ +\\([^:]+\\):\\([0-9]+\\)" 1 2)
            ("\\[\\(.*\\):\\([0-9]+\\)\\]:$" 1 2)
-           ; ("^ *\\[?\\([^:\\n\\r]+\\):\\([0-9]+\\):in" 1 2)
+           ("^ *\\(#{RAILS_ROOT}\\|[[+]\\)?\\([^:
+]+\\):\\([0-9]+\\):in" 2 3)
            ))
-
     (compilation-shell-minor-mode)
     (comint-send-string buffer "autotest\n")))
 
 (defun autotest-switch ()
+  "Switch back and forth between autotest and the previous buffer"
   (interactive)
-  (if (equal "autotest" (buffer-name))
+  (if (equal "*autotest*" (buffer-name))
       (switch-to-buffer nil)
-    (switch-to-buffer "autotest")))
+    (switch-to-buffer "*autotest*")))
 
-;; (defun rails-find-and-goto-error ()
-;;   "Finds error in rails html log go on error line" 
-;;   (interactive)
-;;   (search-forward-regexp "RAILS_ROOT: \\([^<]*\\)")
-;;   (let ((rails-root (concat (match-string 1) "/")))
-;;     (search-forward "id=\"Application-Trace\"")
-;;     (search-forward "RAILS_ROOT}")
-;;     (search-forward-regexp "\\([^:]*\\):\\([0-9]+\\)")
-;;     (let  ((file (match-string 1))
-;;            (line (match-string 2)))
-;;       ;; (kill-buffer (current-buffer))
-;;       (message
-;;        (format "Error found in file \"%s\" on line %s. "  file line))
-;;       (find-file (concat rails-root file))
-;;       (goto-line (string-to-int line)))))
+(if (require 'unit-test nil t)
+    (progn
+      (message "starting emacs server for autotest")
+      (setq unit-test-colours (acons "gray" "#999999" unit-test-colours))
+      (setq unit-test-colours (acons "dark-gray" "#666666" unit-test-colours))
+      (setq unit-test-running-xpm (unit-test-dot "gray"))
+      (server-start)
+      (defun autotest-update (status)
+        "Updates all buffer's modeline with the current test status."
+        (interactive "S")
+        (let ((autotest-map (make-sparse-keymap)))
+          (define-key autotest-map [mode-line mouse-1] 'autotest-switch)
+          (mapcar (lambda (buffer)
+                    (with-current-buffer buffer
+                      (if (eq status 'quit)
+                          (show-test-none)
+                        (progn
+                          (show-test-status status)
+                          (put-text-property
+                           0 3
+                           'keymap autotest-map
+                           (car mode-line-buffer-identification))))))
+                  (buffer-list)))
+        status))
+  (message "unit-test not found, not starting autotest/emacs integration"))
 
 (provide 'autotest)
