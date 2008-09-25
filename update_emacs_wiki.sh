d54c0348b4ee32d853abedd4eadb00ce43552e3a
Ryan Davis <ryand@zenspider.com>
2008-09-25 00:02:05 -0800

    Updated to use cvs/svn export. Slower... but the backups seem dead.
    [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 4233]

--- update_emacs_wiki.sh
+++ update_emacs_wiki.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 
-set -xv
+set -xve
 
-cd "/Users/ryan/Sites/emacs/"
+TMP=/Users/ryan/Sites/emacs/tmp
 
-STATIC="http://www.emacswiki.org/emacs/static.tar.gz"
-STATIC_F="static-$(date +%Y%m%d).tar.gz"
-ELISP="http://www.emacswiki.org/emacs/elisp.tar.gz"
-ELISP_F="elisp-$(date +%Y%m%d).tar.gz"
+mkdir -p $TMP
+cd $TMP
 
-wget -cnv $STATIC -O $STATIC_F &
-wget -cnv $ELISP  -O $ELISP_F  &
+cvs -z3 -d:pserver:anonymous@emacswikicode.cvs.sourceforge.net:/cvsroot/emacswikicode export -f -r HEAD emacswikicode &
+svn export https://svn.rizoma.cl/svn/emacswiki &
 
 wait
+
+cd ..
 
 rm -rf static elisp
 
-tar zxf $STATIC_F &
-tar zxf $ELISP_F  &
+mv tmp/emacswikicode elisp
+mv tmp/emacswiki static
 
-wait
-
-chmod -R a+rX static elisp
-
-find static -name \*.html -print0 | xargs -0 perl -pi -e 's%(=")/cgi-bin/emacs/(?:download/)?%$1../elisp/%g;'
-    
-ls -t static-* | tail +4 | xargs rm -v
-ls -t elisp-*  | tail +4 | xargs rm -v
+rm -rf tmp
