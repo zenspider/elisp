6f8bfaad6f3a4c0f40832376014fc763007db3a2
Ryan Davis <ryand@zenspider.com>
2008-09-25 00:31:27 -0800

    Ignore setup-keys since Drew Adams is a bastard.    Reversed diff arg order.
    [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 4237]

--- find-load-issues.sh
+++ find-load-issues.sh
@@ -3,14 +3,14 @@
 for ff in $(find . -name \*.el); do
     f=$(basename $ff)
     for d in $(emacs --batch --eval "(progn (mapcar 'print load-path) (print (expand-file-name \"~/Sites/emacs/elisp\")))"|sed -n -e 's/\"//gp') /Users/ryan/Work/svn/ruby/ruby_1_8/misc/; do
-        if [ -f $d/$f ]; then
+        if [ -f $d/$f -a $f != setup-keys.el ]; then
             if ! cmp -s $d/$f $ff; then
                 echo "  diff $d/$f $ff"
             fi
         fi
         if [ -f $d/$f.gz ]; then
             echo "  FOUND IN $d/${f}c"
-            zdiff -q $d/$f.gz $ff
+            zdiff -q $ff $d/$f.gz
         fi
     done
 done
