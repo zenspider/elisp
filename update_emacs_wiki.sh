becd713fe4e9e4b6af18fe118b944551fa122b7c
Ryan Davis <ryand@zenspider.com>
2010-11-11 20:27:47 -0800

    gave up and switched to git for static [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 5959]

--- update_emacs_wiki.sh
+++ update_emacs_wiki.sh
@@ -13,10 +13,6 @@ else
     cvs -z3 -d:pserver:anonymous@emacswikicode.cvs.sourceforge.net:/cvsroot/emacswikicode co -f -r HEAD -d elisp emacswikicode &
 fi
 
-# svn sucks because of how much disk space the .svn dir takes up, so
-# we export and rsync in the differences instead. that makes it easier
-# on my backups too.
-TMP=newstatic.noindex
-(rm -rf $TMP; svn export https://svn.rizoma.cl/svn/emacswiki $TMP; rsync -r --delete $TMP/ static; rm -rf $TMP) &
+(cd static; git pull) &
 
 wait
