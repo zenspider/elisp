95f625532e1d2eda0de8d74edec8ca300ba46067
Ryan Davis <ryand@zenspider.com>
2011-01-06 17:31:30 -0800

    removed running-emacs guard. I haven't used xemacs in 8 years [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 6116]

--- hooks/dired-mode.el
+++ hooks/dired-mode.el
@@ -1,4 +1,3 @@
 (define-key dired-mode-map "e" 'wdired-change-to-wdired-mode)
 
-(if running-emacs
-    (define-key dired-mode-map "k" 'dired-kill-subdir))
+(define-key dired-mode-map "k" 'dired-kill-subdir)
