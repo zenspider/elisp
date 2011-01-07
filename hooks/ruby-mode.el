3e2dc773c10954b104450b9608cdfd707318a261
Ryan Davis <ryand@zenspider.com>
2011-01-06 17:30:57 -0800

    Removed which-func mode. It sucks [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 6115]

--- hooks/ruby-mode.el
+++ hooks/ruby-mode.el
@@ -6,11 +6,6 @@
 (define-key ruby-mode-map (kbd "C-c C-b") 'ruby-run-buffer-clean)
 (define-key ruby-mode-map (kbd "C-c C-t") 'ri-show-term-composite-at-point)
 
-;; TODO: fix this to only be the which-func-modes stuff. preferably elsewhere
-(require 'which-func)
-(add-to-list 'which-func-modes 'ruby-mode)
-(which-func-mode 1)
-
 (imenu-add-menubar-index)
 (flyspell-prog-mode)
-(blank-mode))
+(blank-mode)
