54679b41081a10e20d20607a570f03cc2ca905eb
Ryan Davis <ryand@zenspider.com>
2011-06-22 15:06:34 -0800

    Added align-cols to autoloads [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 6336]

--- misc/autoloads.el
+++ misc/autoloads.el
@@ -1,5 +1,6 @@
 ;;;###autoload
 (progn
-  (autoload 'blank-mode                      "blank-mode"      "doco" t)
-  (autoload 'pastebin                        "pastebin"        "doco" t)
-  (autoload 'pastebin-buffer                 "pastebin"        "doco" t))
+  (autoload 'blank-mode      "blank-mode" "doco" t)
+  (autoload 'pastebin        "pastebin"   "doco" t)
+  (autoload 'pastebin-buffer "pastebin"   "doco" t)
+  (autoload 'align-cols (expand-file-name "~/Sites/emacs/elisp/align.el") "doco" t))
