7c9d022af95e3bb7b4d39b0848e7057cbc15c545
Ryan Davis <ryand@zenspider.com>
2011-06-22 15:30:56 -0800

    Added magit Added elpa - tho it seems broken. tweaked hippie-expand-try-functions-list again tweaked out cluck a bit. never quite got the cursor correct, tho I tried... fuckers
    [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 6342]

--- emacs.el
+++ emacs.el
@@ -9,6 +9,7 @@
 
 (add-to-list 'load-path user-init-dir t)
 (add-to-list 'load-path (concat user-init-dir "third-party") t) ; TODO: remove
+(add-to-list 'load-path (concat user-init-dir "third-party/magit") t)
 (add-to-list 'load-path (expand-file-name "~/Sites/emacs/elisp") t)
 
 (defun rwd-recompile-init ()
@@ -36,34 +37,48 @@
     (load autoload-file)
     (message "done loading autoloads")))
 
+(when (load (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (add-to-list 'package-archives
+               '("elpa" . "http://tromey.com/elpa/"))
+  (add-to-list 'package-archives
+               '("marmalade" . "http://marmalade-repo.org/packages/"))
+  (package-initialize))
+
+(defun rwd-install-package (name)
+  (or (package-installed-p name) (package-install name)))
+
 (rwd-autoloads)
 (rwd-autohooks)
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
  '(Info-additional-directory-list (quote ("/usr/share/info" "~/Bin/elisp/info" "/opt/local/share/info/")))
  '(apropos-do-all t)
  '(backup-by-copying-when-linked t)
  '(blank-chars (quote (tabs trailing lines space-before-tab)))
  '(blank-line-length 82)
  '(blank-style (quote (color)))
- '(blink-cursor-mode t)
+ '(blink-cursor-mode nil)
+ '(cluck-fontify-style nil)
+ '(cluck-global-menu-p nil)
  '(column-number-mode t)
  '(comint-input-ignoredups t)
  '(comment-empty-lines (quote (quote eol)))
  '(compilation-error-regexp-alist (quote (bash java gnu gcc-include)))
+ '(completion-styles (quote (basic partial-completion initials emacs22)))
  '(dired-recursive-deletes (quote top))
  '(ediff-split-window-function (quote split-window-horizontally))
+ '(erc-fools (quote ("ianm_")))
  '(erc-modules (quote (autojoin button completion fill irccontrols list match menu move-to-prompt netsplit networks noncommands readonly ring scrolltobottom stamp track)))
  '(eval-expression-print-length nil)
  '(eval-expression-print-level nil)
  '(ffap-file-finder (quote find-file-other-window))
  '(find-file-visit-truename t)
  '(global-auto-revert-mode t)
- '(hippie-expand-try-functions-list (quote (try-expand-all-abbrevs try-expand-list try-expand-dabbrev-visible try-expand-dabbrev try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill try-complete-lisp-symbol-partially try-complete-lisp-symbol try-expand-tag try-complete-file-name-partially try-complete-file-name)))
+ '(hippie-expand-try-functions-list (quote (try-expand-all-abbrevs try-expand-list try-expand-dabbrev-visible try-expand-dabbrev try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill rwd-try-expand-tag try-complete-lisp-symbol-partially try-complete-lisp-symbol try-complete-file-name-partially try-complete-file-name)))
  '(history-length 1000)
  '(imenu-max-items 50)
  '(imenu-sort-function (quote imenu--sort-by-name))
@@ -72,6 +87,7 @@
  '(inhibit-startup-screen t)
  '(initial-scratch-message nil)
  '(line-move-visual nil)
+ '(magit-log-cutoff-length 250)
  '(ns-alternate-modifier (quote none))
  '(ns-command-modifier (quote meta))
  '(ns-pop-up-frames nil)
@@ -82,19 +98,20 @@
  '(pastebin-default-subdomain "zenspider")
  '(pastebin-domain-versions (quote (("pastebin.com" "/api_public.php") ("zenspider.pastebin.com" "/api_public.php") ("pastebin.example.com" "/pastebin.php"))))
  '(read-buffer-completion-ignore-case t)
- '(safe-local-variable-values (quote ((backup-inhibited . t) (racc-token-length-max . 14))))
+ '(safe-local-variable-values (quote ((encoding . utf-8) (backup-inhibited . t) (racc-token-length-max . 14))))
  '(save-place t nil (saveplace))
  '(save-place-limit 100)
  '(save-place-save-skipped nil)
  '(save-place-skip-check-regexp "\\`/\\(cdrom\\|floppy\\|mnt\\|\\([^@/:]*@\\)?[^@/:]*[^@/:.]:\\)")
  '(savehist-ignored-variables (quote (yes-or-no-p-history)))
  '(savehist-mode t nil (savehist))
- '(scheme-program-name "mzscheme")
+ '(scheme-program-name "csi -I ../lib")
  '(scroll-bar-mode nil)
  '(search-whitespace-regexp nil)
  '(sentence-end-double-space nil)
  '(show-paren-mode t)
  '(tab-width 2)
+ '(tags-case-fold-search nil)
  '(tool-bar-mode nil nil (tool-bar))
  '(tooltip-mode nil)
  '(tramp-copy-size-limit 1024)
@@ -102,6 +119,7 @@
  '(transient-mark-mode t)
  '(truncate-partial-width-windows nil)
  '(use-dialog-box nil)
+ '(vc-command-messages t)
  '(vc-handled-backends (quote (CVS SVN GIT)))
  '(vc-p4-require-p4config t)
  '(vc-path (quote ("/opt/local/bin" "/usr/local/bin")))
@@ -110,14 +128,19 @@
  '(warning-suppress-types (quote ((undo discard-info))))
  '(wdired-allow-to-change-permissions (quote advanced)))
 
+;; TODO: figure out why this doesn't work in git-blame frames
+;; '(cursor ((((background light)) (:background "black"))
+;;           (((background dark))  (:background "white"))
+;;           (t                    (:background "white"))))
+
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
  '(blank-line ((((background light)) (:background "gray80")) (((type tty)) (:background "gray20")) (t (:background "red"))))
  '(blank-line-face ((t (:background "gray90"))))
- '(cursor ((t (:background "black"))))
+ '(cursor ((((background light)) (:background "Blue")) (((background dark)) (:background "Blue")) (t (:background "Blue"))))
  '(diff-added ((t (:inherit diff-changed :foreground "green4"))))
  '(diff-removed ((t (:inherit diff-changed :foreground "red4"))))
  '(erc-input-face ((t (:foreground "dark green"))))
@@ -126,3 +149,5 @@
  '(font-lock-comment-face ((((class color) (min-colors 88) (background light)) (:foreground "Dark Blue"))))
  '(font-lock-constant-face ((((class color) (min-colors 88) (background light)) (:foreground "SlateBlue4"))))
  '(font-lock-string-face ((((class color) (min-colors 88) (background light)) (:foreground "Forest Green")))))
+
+(put 'narrow-to-region 'disabled nil)
