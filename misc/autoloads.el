2ed98f5424984c1ca3da90771507820b604bce5a
Ryan Davis <ryand@zenspider.com>
2011-11-25 16:34:59 -0800

    Added lua mode [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 6765]

--- misc/autoloads.el
+++ misc/autoloads.el
@@ -3,4 +3,9 @@
   (autoload 'blank-mode      "blank-mode" "doco" t)
   (autoload 'pastebin        "pastebin"   "doco" t)
   (autoload 'pastebin-buffer "pastebin"   "doco" t)
-  (autoload 'align-cols (expand-file-name "~/Sites/emacs/elisp/align.el") "doco" t))
+  (autoload 'align-cols (expand-file-name "~/Sites/emacs/elisp/align.el") "doco" t)
+
+  (autoload 'lua-mode        "lua-mode" "Lua editing mode." t)
+  (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+  (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+)
