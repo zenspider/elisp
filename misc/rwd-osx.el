a06ce58404ad8a8411e8ff48161a40e53ccf8f85
Ryan Davis <ryand@zenspider.com>
2012-06-06 22:21:03 -0800

    Fixed cd problems in shell by fixing CDPATH env var wrapper [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 7521]

--- misc/rwd-osx.el
+++ misc/rwd-osx.el
@@ -2,13 +2,12 @@
 (setq running-osx (or (featurep 'mac-carbon) (eq 'ns window-system)))
 
 ;;;###autoload
-(if (and running-osx (not (getenv "CDPATH")))
+(if (and running-osx (not (getenv "TERM_PROGRAM")))
     ;; deal with OSX's wonky enivronment by forcing PATH to be correct.
     ;; argh this is stupid
     (let* ((path   (shell-command-to-string "/bin/bash -lc 'echo -n $PATH'"))
-           (cdpath (shell-command-to-string "/bin/bash -lc 'echo -n $CDPATH'"))
+           (cdpath (shell-command-to-string "/bin/bash -ilc 'echo -n $CDPATH'"))
            (path-list (split-string path ":" t)))
       (setenv "PATH" path)
       (setenv "CDPATH" cdpath)
       (dolist (p path-list) (add-to-list 'exec-path p t))))
-
