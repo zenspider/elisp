fde52fd0b42b8eac5b5b657a3f6300c3805c4c6b
Ryan Davis <ryand@zenspider.com>
2012-06-11 14:53:30 -0800

    Added [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 7537]

---
+++ misc/rwd-bookmarks.el
@@ -0,0 +1,5 @@
+;;;###autoload
+(progn
+  ;; To jump to a register, use C-x r j followed by the letter of the register.
+  (set-register ?h '(file . "~/Work/p4/zss/src/hoe/dev/lib/hoe.rb"))
+  (set-register ?u '(file . "~/Work/p4/zss/src/minitest/dev/lib/minitest/unit.rb")))
