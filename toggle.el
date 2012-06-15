2dab7bac2374d663b9a7f1871f7dcc87c72ab0a9
Ryan Davis <ryand@zenspider.com>
2012-06-15 14:10:17 -0800

    minor: fixed doco [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 7556]

--- toggle.el
+++ toggle.el
@@ -43,9 +43,9 @@
 ;; `toggle-mapping-styles' using the `toggle-style' function or set
 ;; your default style via the `toggle-mapping-style' variable.
 
-;; There are 4 different mapping styles in this version: zentest,
-;; rails, and ruby. Feel free to submit more and I'll incorporate
-;; them.
+;; There are 7 different mapping styles in this version: zentest,
+;; rspec, rails, ruby, objc, c, and cpp. Feel free to submit more and
+;; I'll incorporate them.
 
 ;;; Example Mapping (ruby style):
 ;;
