abb7fbc88b41854a412aa3427e29b09719921976
Ryan Davis <ryand@zenspider.com>
2012-07-16 14:49:15 -0800

    Added kill-matching-lines (aliased kill-lines) and rwd-comint-scroll-to-bottom-on-output [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 7641]

--- rwd-aliases.el
+++ rwd-aliases.el
@@ -626,3 +626,32 @@
     (when (and start (re-search-forward end-re))
       (let ((ov (make-overlay start (match-beginning 0))))
         (overlay-put ov 'invisible 'rwd-hide-region)))))
+
+(defun kill-matching-lines (regexp &optional rstart rend interactive)
+  "Kill lines containing matches for REGEXP.
+
+See `flush-lines' or `keep-lines' for behavior of this command.
+
+If the buffer is read-only, Emacs will beep and refrain from deleting
+the line, but put the line in the kill ring anyway.  This means that
+you can use this command to copy text from a read-only buffer.
+\(If the variable `kill-read-only-ok' is non-nil, then this won't
+even beep.)"
+  (interactive
+   (keep-lines-read-args "Kill lines containing match for regexp"))
+  (let ((buffer-file-name nil)) ;; HACK for `clone-buffer'
+    (with-current-buffer (clone-buffer nil nil)
+      (let ((inhibit-read-only t))
+        (keep-lines regexp rstart rend interactive)
+        (kill-region (or rstart (line-beginning-position))
+                     (or rend (point-max))))
+      (kill-buffer)))
+  (unless (and buffer-read-only kill-read-only-ok)
+    ;; Delete lines or make the "Buffer is read-only" error.
+    (flush-lines regexp rstart rend interactive)))
+
+(defalias 'kill-lines 'kill-matching-lines)
+
+(defun rwd-comint-scroll-to-bottom-on-output ()
+  (interactive)
+  (setq comint-scroll-to-bottom-on-output t))
