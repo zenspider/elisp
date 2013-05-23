8a8f01d33ad4fa9d3ad25a6b0e1c987c6de42972
Ryan Davis <ryand-ruby@zenspider.com>
2013-05-22 22:08:10 -0800

    Added comint-info-subjob and bound to C-c C-t [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 8609]

--- hooks/shell-mode.el
+++ hooks/shell-mode.el
@@ -5,3 +5,18 @@
 (define-key comint-mode-map [C-up]            'rwd-previous-line-6)
 (define-key comint-mode-map [C-down]          'rwd-forward-line-6)
 (define-key comint-mode-map (kbd "C-c e")     'rwd-shell-clear)
+
+(defun comint-info-subjob ()
+  "Info the current subjob.
+This command also kills the pending input
+between the process mark and point.
+
+WARNING: if there is no current subjob, you can end up suspending
+the top-level process running in the buffer.  If you accidentally do
+this, use \\[comint-continue-subjob] to resume the process.  (This
+is not a problem with most shells, since they ignore this signal.)"
+  (interactive)
+  (comint-skip-input)
+  (process-send-string (current-buffer) ""))
+
+(define-key comint-mode-map "\C-c\C-t" 'comint-info-subjob)
