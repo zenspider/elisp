8a01607ca0d7f711e57baa061b7b3b444c0f86cf
Ryan Davis <ryand-ruby@zenspider.com>
2013-07-11 13:27:52 -0800

    Added superslow to bookmarks [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 8791]

--- misc/rwd-bookmarks.el
+++ misc/rwd-bookmarks.el
@@ -1,5 +1,6 @@
 ;;;###autoload
 (dolist (pair '((?h "~/Work/p4/zss/src/hoe/dev/lib/hoe.rb")
+                (?w "~/Work/p4/zss/usr/ryand/superslow.txt")
                 (?m "~/Work/p4/zss/src/minitest/dev/lib/minitest.rb")))
   ;; To jump to a register, use C-x r j followed by the letter of the register.
   (set-register (car pair) `(file . ,(cadr pair))))
