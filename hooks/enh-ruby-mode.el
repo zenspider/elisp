e8dfe742fbbec730f12bd919f96dd6a9639e93d6
Ryan Davis <ryand-ruby@zenspider.com>
2013-07-19 12:55:48 -0800

    Moved default mode specs from ruby-mode to enh-ruby-mode.
    [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 8810]

---
+++ hooks/enh-ruby-mode.el
@@ -0,0 +1,60 @@
+;; (inf-ruby-setup-keybindings)
+
+(define-key enh-ruby-mode-map (kbd "C-c C-a") 'autotest-switch)
+(define-key enh-ruby-mode-map (kbd "C-c C-p") 'pastebin)
+(define-key enh-ruby-mode-map (kbd "C-c C-r") 'rcov-buffer)
+(define-key enh-ruby-mode-map (kbd "C-c C-b") 'ruby-run-buffer-clean)
+(define-key enh-ruby-mode-map (kbd "C-c C-t") 'ri-show-term-composite-at-point)
+
+(defun ruby-outline-level ()
+  "Return the depth to which a statement is nested in the outline.
+Point must be at the beginning of a header line.
+This is actually either the level specified in `outline-heading-alist'
+or else the number of characters matched by `outline-regexp'."
+
+  (let ((level (or (and (looking-at outline-regexp)
+                        (match-string 1)
+                        (- (match-end 1) (match-beginning 1)))
+                   0)))
+    (1+ (/ level 2))))
+
+(set (make-local-variable 'outline-level) 'ruby-outline-level)
+
+(setq outline-regexp-ruby
+      (rx (group (* " "))
+          bow
+          (or "BEGIN" "END" "begin" "case" "class" "def" "else" "elsif" "loop"
+              ;; "end"
+              "ensure" "for" "if" "module" "rescue" "unless" "until"
+              "when" "while")
+          eow))
+
+(defun enh-update-outline ()
+  (interactive)
+
+  (set (make-local-variable 'outline-regexp) outline-regexp-ruby))
+
+(enh-update-outline)
+(outline-minor-mode)
+
+(imenu-add-menubar-index)
+(flyspell-prog-mode)
+
+;; enh-ruby-mode hooks into expand-region:
+
+(require 'expand-region-core)
+
+;; (defun er/add-enh-ruby-mode-expansions ()
+;;   "Adds enh-ruby-specific expansions for buffers in enh-ruby-mode"
+;;   (require 'ruby-mode-expansions)
+;;
+;;   (defalias 'er/mark-enh-ruby-instance-variable 'er/mark-ruby-instance-variable)
+;;   (defalias 'er/mark-enh-ruby-block-up          'er/mark-ruby-block-up)
+;;
+;;   (set (make-local-variable 'er/try-expand-list)
+;;        (append
+;;         (remove 'er/mark-defun er/try-expand-list)
+;;         '(er/mark-enh-ruby-instance-variable
+;;           er/mark-enh-ruby-block-up))))
+;;
+;; (er/enable-mode-expansions 'enh-ruby-mode 'er/add-enh-ruby-mode-expansions)
--- modes/enh-ruby-mode.el
+++ modes/enh-ruby-mode.el
@@ -1,7 +1,12 @@
 (autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for ruby files" t)
 
-(add-to-list 'auto-mode-alist        '("\\.rb$" . enh-ruby-mode))
-(add-to-list 'interpreter-mode-alist '("ruby"   . enh-ruby-mode))
+(dolist (spec '(("\\.mab$"   . enh-ruby-mode)
+                ("\\.rb$"    . enh-ruby-mode)
+                ("Rakefile"  . enh-ruby-mode)
+                ("\\.rake$"  . enh-ruby-mode)))
+  (add-to-list 'auto-mode-alist spec))
+
+(add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
 
 (add-to-list 'load-path (expand-file-name "~/Work/git/enhanced-ruby-mode"))
 (setq enh-ruby-program (expand-file-name "~/.multiruby/install/2.0.0-p195/bin/ruby"))
