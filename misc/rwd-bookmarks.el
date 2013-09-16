a535269a30a650bb7cfe7696276c9eb054f1eb4e
Ryan Davis <ryand-ruby@zenspider.com>
2013-09-15 21:29:49 -0800

    sorted and added registers for emacs.el, rwd-keys, and rwd-packages [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 8963]

--- misc/rwd-bookmarks.el
+++ misc/rwd-bookmarks.el
@@ -1,6 +1,9 @@
 ;;;###autoload
-(dolist (pair '((?h "~/Work/p4/zss/src/hoe/dev/lib/hoe.rb")
-                (?w "~/Work/p4/zss/usr/ryand/superslow.txt")
-                (?m "~/Work/p4/zss/src/minitest/dev/lib/minitest.rb")))
+(dolist (pair '((?e "~/Bin/elisp/emacs.el")
+                (?h "~/Work/p4/zss/src/hoe/dev/lib/hoe.rb")
+                (?k "~/Bin/elisp/rwd-keys.el")
+                (?m "~/Work/p4/zss/src/minitest/dev/lib/minitest.rb") ;
+                (?p "~/Bin/elisp/rwd-packages.el")
+                (?w "~/Work/p4/zss/usr/ryand/superslow.txt")))
   ;; To jump to a register, use C-x r j followed by the letter of the register.
   (set-register (car pair) `(file . ,(cadr pair))))
