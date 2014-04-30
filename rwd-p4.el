d9f6c09355c59c1beb317378b3059f5b22185b5f
Ryan Davis <ryand-ruby@zenspider.com>
2014-04-29 16:16:17 -0800

    Updated p4 config for newer version of p4.el [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 9431]

--- rwd-p4.el
+++ rwd-p4.el
@@ -4,5 +4,5 @@
 ;;;###autoload
 (progn
   (when (require 'p4 nil t)
-    (p4-set-client-config ".p4config")
+    (p4-set-p4-config ".p4config")
     (p4-set-p4-executable (expand-file-name "/usr/local/bin/p4"))))
