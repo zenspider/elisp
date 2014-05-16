e5e1c358ffb8ce820b1721e358bc5f50fc20a681
Ryan Davis <ryand-ruby@zenspider.com>
2014-05-16 14:31:31 -0800

    Switching ispell to customize to keep it as close to default as possible. Still not sold on it yet. [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 9472]

--- rwd-text.el
+++ rwd-text.el
@@ -1,11 +1,11 @@
 ;; from http://www.emacswiki.org/emacs/CocoAspell
 ;;;###autoload
-(setq ispell-program-name "aspell"
-      ispell-dictionary "english"
-      ispell-dictionary-alist
-      (let ((default '("[A-Za-z]" "[^A-Za-z]" "[']" nil
-                       ("-B" "-d" "english" "--dict-dir"
-                        "/Library/Application Support/cocoAspell/aspell6-en-6.0-0")
-                       nil iso-8859-1)))
-        `((nil ,@default)
-          ("english" ,@default))))
+;; (setq ispell-program-name "aspell"
+;;       ispell-dictionary "english"
+;;       ispell-dictionary-alist
+;;       (let ((default '("[A-Za-z]" "[^A-Za-z]" "[']" nil
+;;                        ("-B" "-d" "english" "--dict-dir"
+;;                         "/Library/Application Support/cocoAspell/aspell6-en-6.0-0")
+;;                        nil iso-8859-1)))
+;;         `((nil ,@default)
+;;           ("english" ,@default))))
