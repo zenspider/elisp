758d5d692a1146bc6dbdcf5d1672c21510c01a79
Ryan Davis <ryand-ruby@zenspider.com>
2014-07-10 22:43:20 -0800

    Added magit-mode hook [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 9576]

---
+++ hooks/magit-mode.el
@@ -0,0 +1,17 @@
+(defun magit-toggle-whitespace ()
+  (interactive)
+  (if (member "-w" magit-diff-options)
+      (magit-dont-ignore-whitespace)
+    (magit-ignore-whitespace)))
+
+(defun magit-ignore-whitespace ()
+  (interactive)
+  (add-to-list 'magit-diff-options "-w")
+  (magit-refresh))
+
+(defun magit-dont-ignore-whitespace ()
+  (interactive)
+  (setq magit-diff-options (remove "-w" magit-diff-options))
+  (magit-refresh))
+
+(define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
