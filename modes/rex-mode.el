417454ed51d6c3e947911e2b046aa99ba964e0c4
Ryan Davis <ryand-ruby@zenspider.com>
2014-07-10 22:43:11 -0800

    Added mode extensions [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 9575]

---
+++ modes/rex-mode.el
@@ -0,0 +1,2 @@
+(add-to-list 'auto-mode-alist '("\\.rex$"   . rex-mode))
+(autoload 'rex-mode "~/Work/git/rexical/rex-mode.el" "doco" t)
---
+++ modes/shell-mode.el
@@ -0,0 +1,3 @@
+(defun comint-scroll-to-bottom-on-output ()
+  (interactive)
+  (set 'comint-scroll-to-bottom-on-output 'this))
