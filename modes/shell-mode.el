abc3c27c9a7e91fa92916a9a0264e4a3301cd131
Ryan Davis <ryand-ruby@zenspider.com>
2014-07-14 15:16:47 -0800

    how about this? seems to work better [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 9583]

--- modes/shell-mode.el
+++ modes/shell-mode.el
@@ -1,3 +1,7 @@
 (defun comint-scroll-to-bottom-on-output ()
   (interactive)
-  (set 'comint-scroll-to-bottom-on-output 'this))
+  ;; (set 'comint-scroll-to-bottom-on-output 'this)
+
+  (setq comint-scroll-to-bottom-on-input t)
+  (setq comint-scroll-to-bottom-on-output t)
+  (setq comint-move-point-for-output t))
