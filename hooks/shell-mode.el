506d299f3b22b6a453d5ff3be940751918bd65e1
Ryan Davis <ryand-ruby@zenspider.com>
2014-10-17 16:22:22 -0800

    Make C-z C-z be suspend in shell. Allows elscreen to work everywhere [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 9814]

--- hooks/shell-mode.el
+++ hooks/shell-mode.el
@@ -1,6 +1,6 @@
 (setq shell-dirstack-query "dirs -l")
 
-(define-key shell-mode-map (kbd "C-z")        'comint-stop-subjob)
+(define-key shell-mode-map (kbd "C-z C-z")    'comint-stop-subjob)
 (define-key shell-mode-map (kbd "M-<return>") 'shell-resync-dirs)
 (define-key comint-mode-map [C-up]            'rwd-previous-line-6)
 (define-key comint-mode-map [C-down]          'rwd-forward-line-6)
