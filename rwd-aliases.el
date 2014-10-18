566f5d9485337d74a3fbb4067283354592e507f7
Ryan Davis <ryand-ruby@zenspider.com>
2014-10-17 16:21:35 -0800

    AWESOME. dired-ediff-marked-files allows you to ediff via dired [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 9813]

--- rwd-aliases.el
+++ rwd-aliases.el
@@ -861,3 +861,17 @@ narrowed."
         ((region-active-p) (narrow-to-region (region-beginning) (region-end)))
         ((derived-mode-p 'org-mode) (org-narrow-to-subtree))
         (t (narrow-to-defun))))
+
+;;;###autoload
+(defun dired-ediff-marked-files ()
+  ;; from http://www.emacswiki.org/emacs/DavidBoon#toc4
+  "Run ediff on marked ediff files."
+  (interactive)
+  (set 'marked-files (dired-get-marked-files))
+  (when (= (safe-length marked-files) 2)
+    (ediff-files (nth 0 marked-files) (nth 1 marked-files)))
+
+  (when (= (safe-length marked-files) 3)
+    (ediff3 (buffer-file-name (nth 0 marked-files))
+            (buffer-file-name (nth 1 marked-files))
+            (buffer-file-name (nth 2 marked-files)))))
