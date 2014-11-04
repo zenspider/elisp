e46d44b7d303893ee075ea5687c9d8a80f02baac
Ryan Davis <ryand-ruby@zenspider.com>
2014-11-03 21:30:40 -0800

    Added [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 9819]

---
+++ modes/compilation.el
@@ -0,0 +1,13 @@
+;; Helper for compilation. Close the compilation window if
+;; there was no error at all. (emacs wiki)
+(defun compilation-exit-autoclose (status code msg)
+  ;; If M-x compile exists with a 0
+  (when (and (eq status 'exit) (zerop code))
+    ;; then bury the *compilation* buffer, so that C-x b doesn't go there
+    (bury-buffer)
+    ;; and delete the *compilation* window
+    (delete-window (get-buffer-window (get-buffer "*compilation*"))))
+  ;; Always return the anticipated result of compilation-exit-message-function
+  (cons msg code))
+;; Specify my function (maybe I should have done a lambda function)
+(setq compilation-exit-message-function 'compilation-exit-autoclose)
