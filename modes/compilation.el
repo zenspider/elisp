1296c187a85abb61d2c9479d54a1d831f3b8eeb2
Ryan Davis <ryand-ruby@zenspider.com>
2014-11-07 15:55:53 -0800

    Be specific about what compilation window to bury so rgrep and friends work [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 9823]

--- modes/compilation.el
+++ modes/compilation.el
@@ -2,12 +2,11 @@
 ;; there was no error at all. (emacs wiki)
 (defun compilation-exit-autoclose (status code msg)
   ;; If M-x compile exists with a 0
-  (when (and (eq status 'exit) (zerop code))
+  (when (and (eq status 'exit) (zerop code) (get-buffer "*compilation*"))
     ;; then bury the *compilation* buffer, so that C-x b doesn't go there
-    (bury-buffer)
-    ;; and delete the *compilation* window
-    (delete-window (get-buffer-window (get-buffer "*compilation*"))))
+    (bury-buffer "*compilation*")
+    ;; and return to whatever were looking at before
+    (replace-buffer-in-windows "*compilation*"))
   ;; Always return the anticipated result of compilation-exit-message-function
   (cons msg code))
-;; Specify my function (maybe I should have done a lambda function)
 (setq compilation-exit-message-function 'compilation-exit-autoclose)
