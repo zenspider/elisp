beb1609cb410415cb35bb1a00ae008cd60b88def
Ryan Davis <ryand-ruby@zenspider.com>
2014-12-12 18:52:33 -0800

    Added c-mode [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 9877]

---
+++ hooks/c-mode.el
@@ -0,0 +1,7 @@
+(define-key c-mode-map (kbd "C-c C-r") 'recompile)
+(define-key c-mode-map (kbd "C-c C-d") 'delete-trailing-whitespace)
+
+(whitespace-mode)
+
+(setq comment-start "// "
+      comment-end   "")
