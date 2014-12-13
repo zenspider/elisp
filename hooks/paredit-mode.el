28f48a911896e60325c9b6b96e8887fc61875e3c
Ryan Davis <ryand-ruby@zenspider.com>
2014-12-12 18:54:32 -0800

    Documented my paredit hacks [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 9881]

--- hooks/paredit-mode.el
+++ hooks/paredit-mode.el
@@ -1,4 +1,10 @@
+;; paredit-forward-slurp-sexp - also on C-), which is what I use
 (define-key paredit-mode-map (kbd "C-<right>") nil)
+
+;; paredit-forward-barf-sexp - also on C-}, which I apparently never use
 (define-key paredit-mode-map (kbd "C-<left>") nil)
+(define-key paredit-mode-map (kbd "C-M-)") 'paredit-forward-barf-sexp)
+
+;; normally M-s -- no, you don't get that
 (define-key paredit-mode-map (kbd "C-c j") 'paredit-splice-sexp)
 (define-key paredit-mode-map (kbd "M-s") 'fixup-whitespace)
