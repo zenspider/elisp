137a6aa7473bf2851c902f8353f8da2b7eb2c68e
Ryan Davis <ryand-ruby@zenspider.com>
2015-04-01 01:42:02 -0800

    Rearranged recompile and autoload from emacs.el to rwd-autoloads.el.
    [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 10098]

--- emacs.el
+++ emacs.el
@@ -29,38 +29,7 @@ saving keyboard macros (see `edmacro-mode')."
   (load (concat "domain/" domain-name) t) ;; domain/zenspider.com
   (load (concat "host/" host-name) t))    ;; host/greed
 
-(autoload 'find-lisp-find-files "find-lisp" nil t)
-(autoload 'find-lisp-find-files-internal "find-lisp" nil t)
-
-(defun rwd-recompile-init ()
-  (interactive)
-  (byte-recompile-directory (expand-file-name user-init-dir) 0))
-
-(setq generated-autoload-file "loaddefs.el") ;; HACK for emacs 24.1
-
-(defun rwd-autoloads ()
-  "Regenerate the autoload definitions file if necessary and load it."
-  (interactive)
-  (let* ((autoload-file (concat user-init-dir generated-autoload-file)))
-    (if (or (not (file-exists-p autoload-file))
-            (catch 'newer
-              (dolist (file (find-lisp-find-files user-init-dir "\\.el$"))
-                (if (file-newer-than-file-p file autoload-file)
-                    (throw 'newer file)))))
-        (let ((generated-autoload-file autoload-file)
-              (el-root-subdirs (find-lisp-find-files-internal
-                                user-init-dir
-                                'find-lisp-file-predicate-is-directory
-                                'find-lisp-default-directory-predicate)))
-          (apply 'update-directory-autoloads (cons user-init-dir el-root-subdirs))
-          (rwd-recompile-init)
-          (load autoload-file) ; helps rwd-recompile-init dependencies
-
-          ))
-    (message "loading autoloads")
-    (load autoload-file)
-    (message "done loading autoloads")))
-
+(load "rwd-autoloads")
 (load "rwd-packages")
 
 (rwd-autoloads)
--- rwd-autoloads.el
+++ rwd-autoloads.el
@@ -1,7 +1,31 @@
-;;;###autoload
-(progn
-  (autoload 'etags-select-find-tag          "etags-select")
-  (autoload 'etags-select-find-tag-at-point "etags-select")
-  (autoload 'magit-status                   "magit"            nil t)
-  (autoload 'ssh                            "ssh"              nil t)
-  (autoload 'kill-ring-search               "kill-ring-search" nil t))
+(defun rwd-recompile-init ()
+  (interactive)
+  (byte-recompile-directory (expand-file-name user-init-dir) 0))
+
+(setq generated-autoload-file "loaddefs.el") ;; HACK for emacs 24.1
+
+(autoload 'find-lisp-find-files          "find-lisp" nil t)
+(autoload 'find-lisp-find-files-internal "find-lisp" nil t)
+
+(defun rwd-autoloads ()
+  "Regenerate the autoload definitions file if necessary and load it."
+  (interactive)
+  (let* ((autoload-file (concat user-init-dir generated-autoload-file)))
+    (if (or (not (file-exists-p autoload-file))
+            (catch 'newer
+              (dolist (file (find-lisp-find-files user-init-dir "\\.el$"))
+                (if (file-newer-than-file-p file autoload-file)
+                    (throw 'newer file)))))
+        (let ((generated-autoload-file autoload-file)
+              (el-root-subdirs (find-lisp-find-files-internal
+                                user-init-dir
+                                'find-lisp-file-predicate-is-directory
+                                'find-lisp-default-directory-predicate)))
+          (apply 'update-directory-autoloads (cons user-init-dir el-root-subdirs))
+          (rwd-recompile-init)
+          (load autoload-file) ; helps rwd-recompile-init dependencies
+
+          ))
+    (message "loading autoloads")
+    (load autoload-file)
+    (message "done loading autoloads")))
