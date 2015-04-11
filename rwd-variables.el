49733e8f45d92cf5c2d687b592a0a1b15a0151fd
Ryan Davis <ryand-ruby@zenspider.com>
2015-04-10 23:00:04 -0800

    add todo [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 10128]

--- rwd-variables.el
+++ rwd-variables.el
@@ -2,6 +2,7 @@
 ;; ruby -s ./lib/seattlerb_projects.rb -l | pbcopy
 ;;;###autoload
 (defcustom rwd-project-names
+  ;; TODO: make this auto-populate somehow
   '("hoe" "hoe-seattlerb" "ZenTest" "minitest" "minitest_tu_shim" "autotest-rails" "RubyInline" "sexp_processor" "ParseTree" "ruby_parser" "ruby2ruby" "event_hook" "heckle" "flog" "flay" "vlad" "gauntlet" "ruby_to_c" "zenprofile" "wilson" "image_science" "png" "imap_processor" "gmail_contacts" "imap_to_rss" "UPnP-ConnectionManager" "UPnP-MediaServer" "rails_analyzer_tools" "production_log_analyzer" "graph" "ZenWeb" "ZenGraph" "smtp_tls" "autotest-screen" "rubygems-bug" "change_class" "un" "orca_card" "yoda" "SuperCaller" "ZenCallGraph" "seattlerb_dashboard")
   "A list of important project names."
   :group 'rwd
