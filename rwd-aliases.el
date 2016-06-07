577810b99b0031f731f1786439e98d10dd3876ab
Ryan Davis <ryand-ruby@zenspider.com>
2016-06-06 17:29:49 -0800

    Refined ffap advice [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 10702]

--- rwd-aliases.el
+++ rwd-aliases.el
@@ -3,8 +3,8 @@
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;###autoload
-(defadvice find-file-at-point (around goto-line compile activate)
-  (let ((line (and (looking-at ".*?:\\([0-9]+\\)")
+(defadvice find-file-at-point (around goto-line)
+  (let ((line (and (looking-at ".*?:\\([0-9]+\\)\\(:[0-9]+\\)?")
                    (string-to-number (match-string 1)))))
     ad-do-it
     (and line (goto-line line))))
