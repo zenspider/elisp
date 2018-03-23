7070ec8509a1b9f80f489e3808452708b96f1521
Ryan Davis <ryand-ruby@zenspider.com>
2018-03-23 14:58:28 -0800

    Allow compilation output to be kept around via compilation-keep variable [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 11624]

--- modes/compilation.el
+++ modes/compilation.el
@@ -1,8 +1,13 @@
+(defcustom compilation-keep nil "should clean compilations be kept?"
+  :group 'compilation)
+
 ;; Helper for compilation. Close the compilation window if
 ;; there was no error at all. (emacs wiki)
 (defun compilation-exit-autoclose (status code msg)
   ;; If M-x compile exists with a 0
-  (when (and (eq status 'exit) (zerop code) (get-buffer "*compilation*"))
+  (when (and (eq status 'exit) (zerop code)
+             (not compilation-keep)
+             (get-buffer "*compilation*"))
     ;; then bury the *compilation* buffer, so that C-x b doesn't go there
     (bury-buffer "*compilation*")
     ;; and return to whatever were looking at before
