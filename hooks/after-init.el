86348b37149ea72d4f01ccd7bd67a7831112c417
Ryan Davis <ryand-ruby@zenspider.com>
2018-03-23 14:43:14 -0800

    moved stuff from after-init to specific modes [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 11612]

--- hooks/after-init.el
+++ hooks/after-init.el
@@ -1,10 +1,4 @@
 (when (and (boundp 'running-osx) running-osx (string= "/" default-directory))
   (cd "~"))
 
-(global-whitespace-mode 1)
-
 (rwd-load-modes)
-
-(unless window-system
-  (global-set-key (kbd "C-z C-z") 'suspend-frame)
-  (global-set-key (kbd "C-Z") 'suspend-frame)) ; for shell-mode in terms
