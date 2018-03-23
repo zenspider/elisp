36da385c6a8bd5f37d061cc179ae9e697fe29db3
Ryan Davis <ryand-ruby@zenspider.com>
2018-03-23 14:44:30 -0800

    Added binding for dired-ediff-marked-files. Added rwd-toggle-dired-dotfiles. I'd love for this to be ON by default but I haven't figured that one out yet. [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 11613]

--- hooks/dired-mode.el
+++ hooks/dired-mode.el
@@ -1,2 +1,17 @@
 (define-key dired-mode-map "e" 'wdired-change-to-wdired-mode)
 (define-key dired-mode-map "k" 'dired-kill-subdir)
+(define-key dired-mode-map "=" 'dired-ediff-marked-files)
+
+;; Inspired by https://www.emacswiki.org/emacs/DiredOmitMode
+(setq rwd-dired-dotfiles nil)
+
+;; TODO: make this persistent somehow... probably modifying dired-listing-switches
+
+(defun rwd-toggle-dired-dotfiles ()
+  (interactive)
+  (setq rwd-dired-dotfiles (not rwd-dired-dotfiles))
+  (dired-sort-other
+   (apply 'string (remove-if (lambda (c) (and rwd-dired-dotfiles (= ?a c)))
+                             (string-to-list dired-listing-switches)))))
+
+(define-key dired-mode-map "." 'rwd-toggle-dired-dotfiles)
