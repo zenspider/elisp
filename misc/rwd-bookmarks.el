9131b49ba77f4d239ea87c5649b58e43ca442a1a
Ryan Davis <ryand-ruby@zenspider.com>
2018-03-23 14:56:55 -0800

    Added some extra bookmarks [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 11621]

--- misc/rwd-bookmarks.el
+++ misc/rwd-bookmarks.el
@@ -1,9 +1,11 @@
 ;;;###autoload
 (dolist (pair '((?e "~/Bin/elisp/emacs.el")
+                (?o "~/Bin/elisp/hooks")
                 (?h "~/Work/p4/zss/src/hoe/dev/lib/hoe.rb")
                 (?a "~/Bin/elisp/rwd-aliases.el")
                 (?k "~/Bin/elisp/rwd-keys.el")
                 (?m "~/Work/p4/zss/src/minitest/dev/lib/minitest.rb") ;
+                (?s "~/Desktop/steak.org")
                 (?p "~/Bin/elisp/rwd-packages.el")
                 (?w "~/Work/p4/zss/usr/ryand/superslow.txt")))
   ;; To jump to a register, use C-x r j followed by the letter of the register.
