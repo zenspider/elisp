623056314051253350a6a23198bcc8e29a63ca66
Ryan Davis <ryand-ruby@zenspider.com>
2018-06-19 15:35:16 -0800

    Moved all defuns from hooks/magit-mode.el to modes/magit-mode.el. I need to do this to all the other hooks. It's a real PITA to debug a fn that is frozen into a hook. AND: I need a way to unload/reload one of my hook files. Disabled magithub until I figure out their stupid auth bullshit. Maybe never. I haven't really missed it.
    [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 11679]

--- hooks/magit-mode.el
+++ hooks/magit-mode.el
@@ -1,40 +1,9 @@
-(defun magit-toggle-whitespace ()
-  (interactive)
-  (if (member "-w" magit-diff-options)
-      (magit-dont-ignore-whitespace)
-    (magit-ignore-whitespace)))
-
-(defun magit-ignore-whitespace ()
-  (interactive)
-  (add-to-list 'magit-diff-options "-w")
-  (magit-refresh))
-
-(defun magit-dont-ignore-whitespace ()
-  (interactive)
-  (setq magit-diff-options (remove "-w" magit-diff-options))
-  (magit-refresh))
-
 (define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
+(define-key magit-mode-map        (kbd "M-w") nil)
 
 (defadvice magit-visit-item (around always-other-window compile activate)
   (ad-set-arg 0 t)
   ad-do-it)
-
-(define-key magit-mode-map "\M-w" nil)
-
-(require 'json)
-
-(defun yaml-to-json (path)
-  (let* ((rb "ruby -ryaml -rjson -e 'puts JSON.dump YAML.load File.read ARGV.shift' ")
-         (cmd (concat rb path)))
-    (json-read-from-string (shell-command-to-string cmd))))
-
-(defun get-hub-token ()
-  (let* ((json (yaml-to-json "~/.config/hub"))
-         (gh   (alist-get 'github.com json)))
-    (alist-get 'oauth_token (aref gh 0))))
-
-(get-hub-token)
 
 (setq ghub-username "zenspider")
 (setq ghub-token (get-hub-token)) ;; your personal access token
--- modes/magit-mode.el
+++ modes/magit-mode.el
@@ -8,7 +8,37 @@
                      search
                      " `git rev-parse --show-toplevel`")))
 
+(defun magit-toggle-whitespace ()
+  (interactive)
+  (if (member "-w" magit-diff-options)
+      (magit-dont-ignore-whitespace)
+    (magit-ignore-whitespace)))
+
+(defun magit-ignore-whitespace ()
+  (interactive)
+  (add-to-list 'magit-diff-options "-w")
+  (magit-refresh))
+
+(defun magit-dont-ignore-whitespace ()
+  (interactive)
+  (setq magit-diff-options (remove "-w" magit-diff-options))
+  (magit-refresh))
+
+(require 'json)
+
+(defun yaml-to-json (path)
+  (let* ((rb "ruby -ryaml -rjson -e 'puts JSON.dump YAML.load File.read ARGV.shift' ")
+         (cmd (concat rb path)))
+    (condition-case nil
+        (json-read-from-string (shell-command-to-string cmd))
+      (json-error nil))))
+
+(defun get-hub-token ()
+  (let* ((json (yaml-to-json "~/.config/hub"))
+         (gh   (and json (alist-get 'github.com json))))
+    (and gh (alist-get 'oauth_token (aref gh 0)))))
+
 (setq magit-last-seen-setup-instructions "1.4.0")
 
-(require 'magithub)
-(magithub-feature-autoinject t)
+;; (require 'magithub)
+;; (magithub-feature-autoinject t)
