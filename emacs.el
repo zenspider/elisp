77d9a0784f77957cc0e68b3235182dadbb843552
Ryan Davis <ryand-ruby@zenspider.com>
2018-06-19 15:23:52 -0800

    Clean up for newer emacsen: remove kbd defun hack. Clean up os/domain/host initialization. Should be "much" faster. No system calls. Lots of customize added. Trying to migrate more via customize-rogue.
    [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 11677]

--- emacs.el
+++ emacs.el
@@ -1,11 +1,6 @@
-(require 'autoload)                     ; = ;;;###autoload
+;; (package-initialize)
 
-(fmakunbound 'kbd)                      ; HACK for emacs <24.4
-(defun kbd (keys)
-    "Convert KEYS to the internal Emacs key representation.
-KEYS should be a string constant in the format used for
-saving keyboard macros (see `edmacro-mode')."
-      (read-kbd-macro keys))
+(require 'autoload)                     ; = ;;;###autoload
 
 (unless user-init-file                  ; if running w/: -q --debug-init
   (setq user-init-file (expand-file-name "~/.emacs.el")))
@@ -19,11 +14,10 @@ saving keyboard macros (see `edmacro-mode')."
 (add-to-list 'load-path user-init-dir t)
 (add-to-list 'load-path (concat user-init-dir "third-party") t) ; TODO: remove
 
-(let* ((host-cmd "hostname | /usr/bin/perl -pe 's/^.*?([^\.]+\.[^\.]+)$/$1/'")
-       (nl          (string-to-char "\n"))
-       (os-name     (symbol-name system-type))
-       (domain-name (remove nl (shell-command-to-string host-cmd)))
-       (host-name   (car (split-string (system-name) "\\."))))
+(let* ((os-name     (symbol-name system-type))
+       (host-list   (split-string (system-name) "\\."))
+       (host-name   (car host-list))
+       (domain-name (mapconcat 'identity (cdr host-list) ".")))
   (load (concat "os/" os-name) t)         ;; os/darwin
   (load (concat "domain/" domain-name) t) ;; domain/zenspider.com
   (load (concat "host/" host-name) t))    ;; host/greed
@@ -43,18 +37,20 @@ saving keyboard macros (see `edmacro-mode')."
    (quote
     ("/usr/share/info" "~/Bin/elisp/info" "/Developer/usr/share/info")))
  '(apropos-do-all t)
+ '(auto-revert-use-notify nil)
  '(backup-by-copying-when-linked t)
  '(blink-cursor-mode nil)
  '(column-number-mode t)
  '(comint-input-ignoredups t)
  '(comment-empty-lines (quote (quote eol)))
  '(comment-style (quote extra-line))
+ '(company-idle-delay nil)
  '(compilation-error-regexp-alist (quote (bash java gnu gcc-include)))
  '(completion-styles (quote (basic partial-completion initials emacs22)))
  '(dired-recursive-deletes (quote top))
  '(ediff-split-window-function
    (lambda
-     (x)
+     (&optional x)
      (if
          (>
           (frame-width)
@@ -65,11 +61,26 @@ saving keyboard macros (see `edmacro-mode')."
  '(elscreen-display-tab nil)
  '(emerge-diff-options "--ignore-all-space")
  '(enh-ruby-check-syntax nil)
- '(enh-ruby-program "/Users/ryan/.rbenv/versions/2.3.1/bin/ruby")
- '(erc-fools (quote ("Aeyrix" "Hanmac" "yorickpeterse" "ianm_" "Bish")))
+ '(enh-ruby-program "/Users/ryan/.rbenv/versions/2.4.2/bin/ruby")
+ '(erc-fools
+   (quote
+    ("slack-irc-bot" "Aeyrix" "Hanmac" "yorickpeterse" "ianm_" "Bish" "dminuoso" "cthulchu" "Pateros")))
+ '(erc-keywords
+   (quote
+    ("autotest\\|zentest\\|inline\\|parse_?tree\\|minitest\\|parser")))
+ '(erc-kill-buffer-on-part t)
+ '(erc-kill-queries-on-quit t)
+ '(erc-kill-server-buffer-on-quit t)
+ '(erc-mode-line-format "%t %a")
  '(erc-modules
    (quote
     (autojoin button completion fill irccontrols list match menu move-to-prompt netsplit networks noncommands readonly ring scrolltobottom stamp track)))
+ '(erc-server-auto-reconnect nil)
+ '(erc-services-mode t)
+ '(erc-track-exclude-types (quote ("JOIN" "NICK" "PART" "QUIT")))
+ '(erc-track-minor-mode t)
+ '(erc-track-mode t)
+ '(erc-track-position-in-mode-line (quote after-modes))
  '(eval-expression-print-length nil)
  '(eval-expression-print-level nil)
  '(explicit-bash-args (quote ("--noediting" "--login" "-i")))
@@ -81,6 +92,12 @@ saving keyboard macros (see `edmacro-mode')."
  '(fuel-listener-factor-image "/MyApplications/dev/factor/factor.image")
  '(git-commit-summary-max-length 78)
  '(global-auto-revert-mode t)
+ '(golden-ratio-exclude-modes (quote (\'ediff-mode)))
+ '(golden-ratio-inhibit-functions (quote (rwd-golden-ratio-inhibitor)))
+ '(haskell-ask-also-kill-buffers nil)
+ '(haskell-process-auto-import-loaded-modules nil)
+ '(haskell-process-log t)
+ '(haskell-process-suggest-remove-import-lines t)
  '(hippie-expand-try-functions-list
    (quote
     (try-expand-all-abbrevs try-expand-dabbrev-visible try-expand-dabbrev try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill rwd-try-expand-tag try-complete-lisp-symbol-partially try-complete-lisp-symbol try-complete-file-name-partially try-complete-file-name try-expand-list)))
@@ -95,10 +112,20 @@ saving keyboard macros (see `edmacro-mode')."
  '(ispell-extra-args (quote ("\"--sug-mode=fast\"")))
  '(line-move-visual nil)
  '(lua-indent-level 2)
+ '(mac-pass-command-to-system nil)
+ '(mac-pass-control-to-system nil)
  '(magit-diff-refine-hunk t)
  '(magit-emacsclient-executable "/usr/local/bin/emacsclient")
+ '(magit-git-executable "/usr/local/bin/git")
  '(magit-log-cutoff-length 250)
  '(magit-push-always-verify nil)
+ '(magit-repository-directories
+   (quote
+    (("~/Work/git/zenspider" . 1)
+     ("~/Work/git/searbsg" . 1))))
+ '(magithub-api-low-threshold 30)
+ '(magithub-api-timeout 3)
+ '(markdown-fontify-code-blocks-natively t)
  '(mediawiki-site-alist
    (quote
     (("Wikipedia" "http://en.wikipedia.org/w/" "username" "password" "Main Page")
@@ -110,6 +137,9 @@ saving keyboard macros (see `edmacro-mode')."
  '(ns-use-native-fullscreen nil)
  '(oddmuse-directory "~/Library/Caches/oddmuse")
  '(oddmuse-username "RyanDavis")
+ '(org-display-custom-times t)
+ '(org-time-stamp-custom-formats (quote ("<%Y-%m-%d>" . "<%Y-%m-%d %H:%M>")))
+ '(org-use-sub-superscripts (quote {}))
  '(outline-minor-mode-prefix "\357")
  '(override-keymap-rules
    (quote
@@ -118,31 +148,29 @@ saving keyboard macros (see `edmacro-mode')."
      ("\214" rwd-scroll-top
       (shell comint)))))
  '(p4-do-find-file nil)
- '(pastebin-default-domain "zenspider.pastebin.com")
- '(pastebin-default-subdomain "zenspider")
- '(pastebin-domain-versions
+ '(package-selected-packages
    (quote
-    (("pastebin.com" "/api_public.php")
-     ("zenspider.pastebin.com" "/api_public.php")
-     ("pastebin.example.com" "/pastebin.php"))))
- '(quack-default-program "racket")
- '(quack-pltcollect-dirs (quote ("/MyApplications/dev/lisp/Racket/collects/")))
+    (lsp-haskell flycheck-popup-tip json-mode json-reformat json-snatcher jq-mode magit-gh-pulls let-alist string-edit boxquote rebox2 unicode-enbox yaml-mode yagist window-number wgrep-ag web-mode w3m swift-mode ssh sml-mode smart-jump shm shell-command racket-mode popwin phi-search-mc phi-grep perspective package+ outline-magic nand2tetris names markdown-mode magit-p4 litable kill-ring-search keyfreq inf-ruby htmlize hindent golden-ratio github-browse-file git-timemachine geiser find-file-in-project expand-region eval-in-repl elscreen-buffer-group elisp-slime-nav dockerfile-mode dante company bts-github browse-kill-ring bison-mode aggressive-indent ag)))
+ '(racket-images-system-viewer "open")
  '(read-buffer-completion-ignore-case t)
  '(safe-local-variable-values
    (quote
-    ((ffip-project-file . "Rakefile")
+    ((lexical . t)
+     (ffip-project-root . "/Users/ryan/Work/p4/zss/src/minitest/dev/")
+     (ffip-project-file . "Rakefile")
      (ffip-prune-patterns . "tmp/isolate")
      (encoding . utf-8)
      (backup-inhibited . t)
      (racc-token-length-max . 14))))
  '(save-interprogram-paste-before-kill t)
- '(save-place t nil (saveplace))
  '(save-place-file "~/.emacs.d/emacs-places")
  '(save-place-limit 100)
  '(save-place-save-skipped nil)
  '(save-place-skip-check-regexp
    "\\`/\\(cdrom\\|floppy\\|mnt\\|\\([^@/:]*@\\)?[^@/:]*[^@/:.]:\\)")
- '(savehist-additional-variables (quote (kill-ring)))
+ '(savehist-additional-variables
+   (quote
+    (kill-ring compile-command search-ring regexp-search-ring)))
  '(savehist-ignored-variables (quote (yes-or-no-p-history)))
  '(savehist-mode t nil (savehist))
  '(scheme-program-name "csi -I ../lib")
@@ -156,6 +184,7 @@ saving keyboard macros (see `edmacro-mode')."
  '(smtpmail-smtp-service 25)
  '(smtpmail-smtp-user "ryan")
  '(sql-sqlite-program "sqlite3")
+ '(tab-always-indent (quote complete))
  '(tab-width 8)
  '(tags-case-fold-search nil)
  '(tags-revert-without-query t)
@@ -164,10 +193,10 @@ saving keyboard macros (see `edmacro-mode')."
  '(tramp-copy-size-limit 1024)
  '(tramp-default-method "rsync")
  '(tramp-use-ssh-controlmaster-options nil)
- '(transient-mark-mode (quote (only . t)))
  '(truncate-partial-width-windows nil)
  '(use-dialog-box nil)
  '(user-mail-address "ryand-ruby@zenspider.com")
+ '(vc-annotate-display-mode (quote scale))
  '(vc-command-messages t)
  '(vc-handled-backends (quote (CVS SVN GIT)))
  '(vc-p4-require-p4config t)
@@ -175,9 +204,10 @@ saving keyboard macros (see `edmacro-mode')."
  '(visible-bell t)
  '(warning-suppress-types (quote ((undo discard-info))))
  '(wdired-allow-to-change-permissions (quote advanced))
+ '(web-mode-markup-indent-offset 2)
  '(whitespace-global-modes
    (quote
-    (ruby-mode emacs-lisp-mode scheme-mode enh-ruby-mode c-mode racket-mode sml-mode)))
+    (ruby-mode emacs-lisp-mode scheme-mode enh-ruby-mode c-mode racket-mode sml-mode racc-mode rust-mode diff-mode haskell-mode text-mode)))
  '(whitespace-style
    (quote
     (face tabs trailing lines-tail space-before-tab empty))))
@@ -195,6 +225,12 @@ saving keyboard macros (see `edmacro-mode')."
  '(cursor ((((type ns)) (:background "Blue")) (((type tty)) (:background "Green")) (t (:background "Blue"))))
  '(diff-added ((t (:inherit diff-changed :foreground "green4"))))
  '(diff-removed ((t (:inherit diff-changed :foreground "red4"))))
+ '(ediff-current-diff-C ((t (:background "#ddddff"))))
+ '(ediff-even-diff-B ((t (:background "Grey" :foreground "black"))))
+ '(ediff-fine-diff-A ((t (:background "#ffbbbb" :foreground "black"))))
+ '(ediff-fine-diff-C ((t (:background "#aaaaff" :foreground "black"))))
+ '(ediff-odd-diff-A ((t (:background "Grey" :foreground "black"))))
+ '(ediff-odd-diff-C ((t (:background "Grey" :foreground "black"))))
  '(enh-ruby-heredoc-delimiter-face-xxx ((t (:foreground "Brown"))))
  '(erc-input-face ((t (:foreground "dark green"))))
  '(erc-my-nick-face ((t (:foreground "dark green" :weight bold))))
@@ -207,6 +243,9 @@ saving keyboard macros (see `edmacro-mode')."
  '(mode-line-inactive ((t (:inherit mode-line :background "grey90" :foreground "grey20" :weight light))))
  '(racket-keyword-argument-face ((t (:foreground "dark blue"))))
  '(region ((((type tty)) (:background "color-240"))))
+ '(sh-heredoc ((t (:foreground "DarkOliveGreen4"))))
+ '(shm-current-face ((t (:background "#eee"))))
+ '(shm-quarantine-face ((t (:background "#fcc"))))
  '(whitespace-line ((((type ns)) (:background "gray90")) (((type tty)) (:background "gray20")) (t (:background "red"))))
  '(whitespace-tab ((((type tty)) (:background "grey22")) (((type ns)) (:background "beige")) (t (:background "beige")))))
 
