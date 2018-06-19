64ae8a1da60916accd842dc801deb1acad560e95
Ryan Davis <ryand-ruby@zenspider.com>
2018-06-19 15:21:00 -0800

    Refinements no racket-mode hook. Mostly indentation. racket-run now clears the buffer. Can't figure out how to nuke the repl quickly. [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 11676]

--- hooks/racket-mode.el
+++ hooks/racket-mode.el
@@ -1,37 +1,56 @@
 (paredit-mode +1)
-(aggressive-indent-mode)
 
+(define-key racket-mode-map (kbd "C-c u")       'racket-unicode-input-method-enable)
 (define-key racket-mode-map (kbd "C-c C-r")     'racket-run)
+(define-key racket-mode-map (kbd "C-c r")       'racket-run)
 (define-key racket-mode-map (kbd "C-c t")       'racket-test)
-(define-key racket-mode-map (kbd "C-c C-c C-d") 'racket-doc)
 (define-key racket-mode-map (kbd "C-c C-d")     'delete-trailing-whitespace)
+;; (define-key racket-mode-map (kbd "C-c C-c C-d") 'racket-doc)
 
 (defun paredit-space-for-delimiter-predicates-scheme (endp delimiter)
   "Do not automatically insert a space when a '#' precedes parentheses."
   (or endp
       (cond ((eq (char-syntax delimiter) ?\")
-             (not (looking-back "#\\|#hash\\|#rx\\|#px")))
+             (not (looking-back "#\\|#hash\\|#rx\\|#px" nil)))
             (t t))))
 
-;; (let ((defines  '(struct local struct: define-struct: ...
-;;       (begins   '(case-lambda case-lambda: pcase-lambda: ...
-;;       (lambdas  '(cases instantiate super-instantiate syntax/loc ...
-;;       (specials '(for/fold for/fold:)))
+(add-to-list 'paredit-space-for-delimiter-predicates
+             'paredit-space-for-delimiter-predicates-scheme)
 
-(let ((defines  '())
-      (begins   '(check-run* cond-e cond-a cond-u))
+(let ((defines (append
+                '(reduction-relation)
+                (mapcar (lambda (n) (intern (make-string n ?-))) ; redex
+                        (number-sequence 3 10))))
+      (begins   '(check-run* cond-e cond-a cond-u
+                             check-equal? test-equal))
       (lambdas  '(λg λf test-case test-suite call-with-output-file*
                      lambdag@ lambdaf@ exists exist nom project run* run1
                      run2 run3 run4 run5 run6 run7 run8 run9 run10
-                     case∞ case-inf take fresh))
-      (specials '(run)))
+                     case∞ case-inf take fresh
+                     pattern-case-filter))
+      (specials '((for/fold for/fold/1)
+                  (for/list for/list/flat)
+                  (for/or for/all for/none)
+                  (for/sum for/count)
+                  (do run))))
 
   (mapc (lambda (name) (put name 'racket-indent-function 'defun)) defines)
   (mapc (lambda (name) (put name 'racket-indent-function 0))      begins)
   (mapc (lambda (name) (put name 'racket-indent-function 1))      lambdas)
-  (mapc (lambda (name) (put name 'racket-indent-function 2))      specials))
 
-;; (put 'fresh 'racket-indent-function 4)
+  (mapc (lambda (txs)
+          (let* ((type (car txs))
+                 (xs (cdr txs))
+                 (dent (get type 'racket-indent-function)))
+            (mapc (lambda (x)
+                    (put x 'racket-indent-function dent))
+                  xs)))
+        specials))
 
-(add-to-list 'paredit-space-for-delimiter-predicates
-             'paredit-space-for-delimiter-predicates-scheme)
+(defadvice racket-test (before racket-test-clear compile activate)
+  (with-racket-repl-buffer
+    (rwd-shell-clear)))
+
+(defadvice racket-run (before racket-run-clear compile activate)
+  (with-racket-repl-buffer
+    (rwd-shell-clear)))
