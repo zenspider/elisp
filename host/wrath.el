ac25954fdf79dcfeb4804ef5e69b597b5466b417
Ryan Davis <ryand-ruby@zenspider.com>
2019-03-31 14:25:20 -0800

    Only fire off rwd-lappy if emacs is fired up w/ no args. [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 11916]

--- host/wrath.el
+++ host/wrath.el
@@ -1,2 +1,6 @@
+(defun rwd/after-init/lappy ()
+  (when (eq 1 (length command-line-args))
+    (rwd-lappy)))
+
 (when window-system
- (add-hook 'after-init-hook (lambda () (rwd-lappy)) t))
+ (add-hook 'after-init-hook 'rwd/after-init/lappy t))
