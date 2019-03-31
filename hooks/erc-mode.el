5d1af9e102a8e9a83e40813e123052bbb1c990b7
Ryan Davis <ryand-ruby@zenspider.com>
2019-03-31 14:29:24 -0800

    Switch to 'i' perspective when going to erc. Uses new advice-add. Me likey. [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 11920]

--- hooks/erc-mode.el
+++ hooks/erc-mode.el
@@ -1,2 +1,9 @@
 (remove-hook 'erc-text-matched-hook 'erc-hide-fools)
 (easy-menu-add-item  nil '("tools") ["IRC" erc-select t])
+
+(defun rwd/erc-track-switch-buffer+perspective (&rest args)
+  (cond (erc-modified-channels-alist
+         (persp-switch "i"))))
+
+(advice-add 'erc-track-switch-buffer
+            :before #'rwd/erc-track-switch-buffer+perspective)
