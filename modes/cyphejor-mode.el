e5eb10337067d9a573ffd479f5985dae76cbac51
Ryan Davis <ryand-ruby@zenspider.com>
2019-03-31 14:27:43 -0800

    Clean up the modeline noise using cyphejor and delight. I want to merge these two [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 11919]

---
+++ modes/cyphejor-mode.el
@@ -0,0 +1,25 @@
+(require 'cyphejor)
+
+(setq cyphejor-rules '(:downcase
+                       ("emacs"       "ε")
+                       ("inferior"    "i"  :prefix)
+                       ("interaction" "i"  :prefix)
+                       ("interactive" "i"  :prefix)
+                       ("lisp"        "λ"  :postfix)
+                       ("mode"        "")
+                       ("shell"       "sh" :postfix)
+                       ))
+
+(cyphejor-mode 1)
+
+;; TODO: I hate that I need two modes to do one simple thing.
+;; I MUCH prefer the approach above, but it doesn't do minor-modes. Extend?
+
+(require 'delight)
+(delight '((abbrev-mode            nil  "abbrev")
+           (eldoc-mode             nil  "eldoc")
+           (elisp-slime-nav-mode   nil  "elisp-slime-nav")
+           (flyspell-mode          nil  "flyspell")
+           (global-whitespace-mode nil  "whitespace")
+           (outline-minor-mode     nil  "outline")
+           (paredit-mode           nil  "paredit")))
