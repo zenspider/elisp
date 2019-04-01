570156b34aa3da70c01450f9480266399dbb0c47
Ryan Davis <ryand-ruby@zenspider.com>
2019-04-01 00:10:00 -0800

    added todos to fix emacs-lisp-mode [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 11926]

--- hooks/emacs-lisp-mode.el
+++ hooks/emacs-lisp-mode.el
@@ -1,3 +1,7 @@
 (paredit-mode 1)
 ;; (aggressive-indent-mode)
 (turn-on-elisp-slime-nav-mode)
+
+;; TODO: remap these and bring back my binding for delete-trailing-whitespace
+;; C-c C-d C-d     elisp-slime-nav-describe-elisp-thing-at-point
+;; C-c C-d d       elisp-slime-nav-describe-elisp-thing-at-point
