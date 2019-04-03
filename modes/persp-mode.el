3100d112f06c46ae459cfc8ce07f53b9447199d4
Ryan Davis <ryand-ruby@zenspider.com>
2019-04-02 16:04:35 -0800

    fixed suspension for terminal + persp-mode [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 11942]

--- modes/persp-mode.el
+++ modes/persp-mode.el
@@ -1,2 +1,6 @@
 (define-key perspective-map (kbd "d") 'persp-kill)
 (define-key perspective-map (kbd "RET") 'persp-switch-last)
+
+(unless window-system                   ; restore C-z z and C-z C-z to suspend
+  (define-key perspective-map (kbd "z")   'suspend-frame)
+  (define-key perspective-map (kbd "C-z") 'suspend-frame))
