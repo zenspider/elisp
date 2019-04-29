208810c6c57e1b3d87cba5e8d91c9212dcda828e
Ryan Davis <ryand-ruby@zenspider.com>
2019-04-29 14:26:59 -0800

    More shell-mode improvements!
    + Default to fitering ansi-colors. Bundler's bullshit yellow on white is dead.
    + Moved comint/shell functions to modes/shell-mode.el.
    + Added rwd-comint-show-full-color for when I need it.
    [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12027]

--- hooks/shell-mode.el
+++ hooks/shell-mode.el
@@ -1,28 +1,12 @@
-(require 'xterm-color)
-(setq comint-output-filter-functions
-      (remove 'ansi-color-process-output comint-output-filter-functions))
-(add-hook 'comint-preoutput-filter-functions 'xterm-color-filter nil t)
+(setq ansi-color-for-comint-mode 'filter)
 
 (setq shell-dirstack-query "dirs -l")
 
 (define-key shell-mode-map (kbd "C-c C-d") nil)  ; keeps biting me
 (define-key shell-mode-map (kbd "C-z C-z")    'comint-stop-subjob)
 (define-key shell-mode-map (kbd "M-<return>") 'shell-resync-dirs)
-(define-key comint-mode-map [C-up]            'rwd-previous-line-6)
-(define-key comint-mode-map [C-down]          'rwd-forward-line-6)
+
+(define-key comint-mode-map (kbd "C-<up>")    'rwd-previous-line-6)
+(define-key comint-mode-map (kbd "C-<down>")  'rwd-forward-line-6)
 (define-key comint-mode-map (kbd "C-c e")     'rwd-shell-clear)
-
-(defun comint-info-subjob ()
-  "Info the current subjob.
-This command also kills the pending input
-between the process mark and point.
-
-WARNING: if there is no current subjob, you can end up suspending
-the top-level process running in the buffer.  If you accidentally do
-this, use \\[comint-continue-subjob] to resume the process.  (This
-is not a problem with most shells, since they ignore this signal.)"
-  (interactive)
-  (comint-skip-input)
-  (process-send-string (current-buffer) ""))
-
-(define-key comint-mode-map "\C-c\C-t" 'comint-info-subjob)
+(define-key comint-mode-map (kbd "C-c C-t")   'rwd/comint-info-subjob)
--- modes/shell-mode.el
+++ modes/shell-mode.el
@@ -1,7 +1,28 @@
 (defun comint-scroll-to-bottom-on-output ()
   (interactive)
-  ;; (set 'comint-scroll-to-bottom-on-output 'this)
-
   (setq comint-scroll-to-bottom-on-input t)
   (setq comint-scroll-to-bottom-on-output t)
   (setq comint-move-point-for-output t))
+
+(defun rwd/comint-show-full-color ()
+  (interactive)
+  (font-lock-mode -1)
+  (require 'xterm-color)
+  (make-local-variable 'comint-output-filter-functions)
+  (make-local-variable 'comint-preoutput-filter-functions)
+  (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter nil t)
+  (remove-from-list comint-output-filter-functions 'ansi-color-process-output)
+  (remove-from-list comint-output-filter-functions 'comint-postoutput-scroll-to-bottom))
+
+(defun rwd/comint-info-subjob ()
+  "Info the current subjob.
+This command also kills the pending input
+between the process mark and point.
+
+WARNING: if there is no current subjob, you can end up suspending
+the top-level process running in the buffer.  If you accidentally do
+this, use \\[comint-continue-subjob] to resume the process.  (This
+is not a problem with most shells, since they ignore this signal.)"
+  (interactive)
+  (comint-skip-input)
+  (process-send-string (current-buffer) ""))
