0d4e6d36a77fc84a10d264877534744716bb17f6
Ryan Davis <ryand-ruby@zenspider.com>
2019-04-29 14:21:05 -0800

    Tweaked perspective for better sorting [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12026]

--- modes/bs-mode.el
+++ modes/bs-mode.el
@@ -3,9 +3,15 @@
 ;; adapted from:
 ;; https://github.com/gcv/dotfiles/blob/master/emacs/init.el
 
+(defun rwd/persp/current-buffers ()
+  (persp-buffers (persp-curr)))
+
 (defun rwd/bs-config/persp (buf)
   (with-current-buffer buf
-    (not (member buf (persp-buffers (persp-curr))))))
+    (not (memq buf (rwd/persp/current-buffers)))))
 
 ;; TODO: switch to customize when I get this right
-(add-to-list 'bs-configurations '("persp" nil nil "^\\*" rwd/bs-config/persp))
+(add-to-list 'bs-configurations '("persp" nil nil
+                                  "^\\*"
+                                  rwd/bs-config/persp
+                                  bs-sort-buffer-interns-are-last))
