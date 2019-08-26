4b98297a9dc9ac2a7e3400869dc7ef25641690a4
Ryan Davis <ryand-ruby@zenspider.com>
2019-08-26 15:20:34 -0800

    Nope! I did NOT like it! Go back to a regular 3-way split of the current window [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12210]

--- rwd-aliases.el
+++ rwd-aliases.el
@@ -354,10 +354,8 @@
   "Splits the current frame horizontally into even thirds."
   (interactive)
   (delete-other-windows)
-  (let ((win/a (split-window-horizontally))
-        (win/b (split-window-horizontally)))
-    (set-window-buffer win/a (other-buffer))
-    (set-window-buffer win/b (other-buffer (other-buffer))))
+  (split-window-horizontally)
+  (split-window-horizontally)
   (balance-windows))
 
 ;;;###autoload
