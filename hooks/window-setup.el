aa8affb615e3b9f692e27ef463789624e7170290
Ryan Davis <ryand-ruby@zenspider.com>
2019-08-26 15:18:45 -0800

    reset the display after windows are all set up. Allows for size changes for monitors [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12208]

--- hooks/window-setup.el
+++ hooks/window-setup.el
@@ -4,3 +4,6 @@
 (message "Emacs ready in %.2f seconds with %d garbage collections."
          (float-time (time-subtract after-init-time before-init-time))
          gcs-done)
+
+(when normal-gui-startup
+  (rwd/display-reset))                  ; re-trigger dispwatch
