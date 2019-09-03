3abf7bdd4ef62a6767dc074e74c5ff786c4605ec
Ryan Davis <ryand-ruby@zenspider.com>
2019-09-02 17:39:36 -0800

    resize full and don't create a shell on window-setup (let persp deal with it) [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12222]

--- hooks/window-setup.el
+++ hooks/window-setup.el
@@ -1,9 +1,9 @@
 (when normal-gui-startup
-  (rwd-lappy))
+  (rwd-resize-full))
 
 (message "Emacs ready in %.2f seconds with %d garbage collections."
          (float-time (time-subtract after-init-time before-init-time))
          gcs-done)
 
 (when normal-gui-startup
-  (rwd/display-reset))                  ; re-trigger dispwatch
+  (rwd/display-reset))                  ; trigger dispwatch to get font changes
