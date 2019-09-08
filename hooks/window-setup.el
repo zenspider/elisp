6c64d76bd4117aa176b3b8d750fba0152c19520e
Ryan Davis <ryand-ruby@zenspider.com>
2019-09-08 03:31:57 -0800

    cleaned window-setup hook by triggering display-change directly. [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12234]

--- hooks/window-setup.el
+++ hooks/window-setup.el
@@ -1,9 +1,7 @@
 (when normal-gui-startup
-  (rwd-resize-full))
+  (rwd-resize-full)
+  (rwd/display-change dispwatch-current-display))
 
 (message "Emacs ready in %.2f seconds with %d garbage collections."
          (float-time (time-subtract after-init-time before-init-time))
          gcs-done)
-
-(when normal-gui-startup
-  (rwd/display-reset))                  ; trigger dispwatch to get font changes
