58831504152404bb1d833fbe33f7c14d1c794a23
Ryan Davis <ryand-ruby@zenspider.com>
2019-09-08 03:30:23 -0800

    Switched to assoc list for rwd/display-change. [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12232]

--- modes/dispwatch.el
+++ modes/dispwatch.el
@@ -1,18 +1,25 @@
+(defvar rwd/displays
+  `(("1440x900"  ,(* 1 80) ,(* 2 80) "Menlo" 14)
+    ("4000x1440" ,(* 2 60) ,(* 2 80) "Menlo" 16))
+  "Contains a list of display entries for emacs' dispwatch")
+
+(defun rwd/display-setup (h w f s)
+  (setq split-height-threshold h
+        split-width-threshold w)
+  (rwd-set-mac-font f s)
+  (message "rwd/display-setup %s %s %s %s" h w f s))
+
 (defun rwd/display-change (disp)
   (interactive)
-  (cond ((equal disp "1440x900")
-         (setq split-height-threshold 80 ; the defaults
-               split-width-threshold (* 2 80))
-         (rwd-set-mac-font "Menlo" 14))
-        ((equal disp "4000x1440")
-         (setq split-height-threshold (* 2 60)
-               split-width-threshold (* 2 80))
-         (rwd-set-mac-font "Menlo" 16))
-        (t (find-function 'rwd/display-change)
-           (error "Please add %S to rwd/display-change" disp)))
-  (rwd-fix-fullscreen)
-  (sleep-for 0.5)
-  (rwd-split-smart))
+  (message "rwd/display-change %s" disp)
+  (let ((args (assoc disp rwd/displays)))
+    (unless args
+      (find-variable 'rwd/displays)
+      (error "Please extend rwd/displays with %S" disp))
+    (apply 'rwd/display-setup (cdr args))
+    (rwd-fix-fullscreen)
+    (sleep-for 0.25)
+    (rwd-split-smart)))
 
 ;; https://github.com/mnp/dispwatch
 (require 'dispwatch)
