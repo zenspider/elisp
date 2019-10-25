9de08582e55ef18a021429047815aac180492a74
Ryan Davis <ryand-ruby@zenspider.com>
2019-10-24 18:45:25 -0800

    Fixed ssh-mode dirtrack problems I have been having for a while. Yay! [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12362]

--- hooks/ssh-mode.el
+++ hooks/ssh-mode.el
@@ -1,1 +1,7 @@
-(ssh-directory-tracking-mode)
+;; (ssh-directory-tracking-mode) -- causes an error on connection
+
+(shell-dirtrack-mode t)
+
+(setq ssh-directory-tracking-mode t)
+(setq shell-dirtrackp t)
+(setq comint-file-name-prefix (make-comint-file-name-prefix))
