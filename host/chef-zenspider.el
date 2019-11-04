1e6f43148bbb9bacc6a5dbc83833902b382b4390
Ryan Davis <ryand-ruby@zenspider.com>
2019-11-04 13:06:40 -0800

    Moved magit-refs-filter-alist to chef-zenspider.el [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12387]

--- host/chef-zenspider.el
+++ host/chef-zenspider.el
@@ -3,3 +3,7 @@
              (flycheck-mode 1)
              (define-key enh-ruby-mode-map (kbd "C-c e")
                'rwd-flycheck-toggle-list-errors)))
+
+(add-hook 'magit-mode-hook
+          '(lambda ()
+             (add-to-list 'magit-refs-filter-alist '("^[v0-9]"))))
