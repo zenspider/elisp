982e92b310539f234300d45e197a3133d4a5cc6a
Ryan Davis <ryand-ruby@zenspider.com>
2019-12-02 15:17:10 -0800

    Pushed up a fair amount of code to perspective and cleaned up my bs setup [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12420]

--- modes/bs-mode.el
+++ modes/bs-mode.el
@@ -3,29 +3,19 @@
 ;; adapted from:
 ;; https://github.com/gcv/dotfiles/blob/master/emacs/init.el
 
-(defun rwd/persp/current-buffers ()
-  (persp-buffers (persp-curr)))
-
 (defun rwd/buffer-major-mode (&optional buf)
   (with-current-buffer (or buf (current-buffer))
     major-mode))
 
-(defun rwd/bs-config/persp (buf)
-  (with-current-buffer buf
-    (memq buf (rwd/persp/current-buffers))))
-
-(defun rwd/bs-config/not-persp (buf)
-  (not (rwd/bs-config/persp buf)))
-
 (defun rwd/persp/same-mode (current-buf)
   (let* ((orig-mode    (rwd/buffer-major-mode bs--buffer-coming-from))
          (current-mode (rwd/buffer-major-mode current-buf)))
-    (and (rwd/bs-config/persp current-buf)
+    (and (persp-is-current-buffer current-buf)
          (equal orig-mode current-mode))))
 
 (add-to-list 'bs-configurations '("persp"
                                   nil nil
-                                  "^\\*" rwd/bs-config/not-persp
+                                  "^\\*" persp-buffer-filter
                                   bs-sort-buffer-interns-are-last))
 
 (add-to-list 'bs-configurations '("current-mode"
