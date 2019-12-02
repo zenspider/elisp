84c2266d943a3e5d92c50f7d5c2b1fbfb18233d7
Ryan Davis <ryand-ruby@zenspider.com>
2019-12-02 15:19:14 -0800

    THIS ONE IS AWESOME: if the perspective name is uppercase and is NOT a link to a directory then ask for a directory. [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12422]

--- hooks/persp-created.el
+++ hooks/persp-created.el
@@ -1,8 +1,12 @@
 (require 'f)
 
-(let ((curr-name (persp-name (persp-curr))))
-  (let ((proj-dir (concat (f-canonical (f-join "~/Links" curr-name)) "/")))
-    (with-current-buffer (persp-scratch-buffer)
-      (when (f-directory? proj-dir)
-        (setq default-directory proj-dir))
-      (rwd-shell))))
+(let* ((curr-name (persp-current-name))
+       (maybe-dir (concat (f-canonical (f-join "~/Links" curr-name)) "/"))
+       (proj--dir
+        (cond ((f-directory? maybe-dir) maybe-dir)
+              ((s-lowercase? curr-name) default-directory)
+              (t (call-interactively
+                  #'(lambda (path) (interactive "Glocation: ") path))))))
+  (with-current-buffer (persp-scratch-buffer)
+    (setq default-directory proj--dir)
+    (rwd-shell)))
