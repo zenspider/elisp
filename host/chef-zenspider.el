8e131c264fe1bda50b4714e643e1c339d351e67f
Ryan Davis <ryand-ruby@zenspider.com>
2019-12-09 14:51:41 -0800

    I'm getting annoyed with flycheck on work lappy, so make it disableable [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12432]

--- host/chef-zenspider.el
+++ host/chef-zenspider.el
@@ -1,9 +1,13 @@
-(add-hook 'enh-ruby-mode-hook
-          '(lambda ()
-             (flycheck-mode 1)
-             (define-key enh-ruby-mode-map (kbd "C-c e")
-               'rwd-flycheck-toggle-list-errors)))
+(defun zenspider/erm/flycheck ()
+  (flycheck-mode 1)
+  (define-key enh-ruby-mode-map (kbd "C-c e")
+    'rwd-flycheck-toggle-list-errors))
 
-(add-hook 'magit-mode-hook
-          '(lambda ()
-             (add-to-list 'magit-refs-filter-alist '("^[v0-9]"))))
+(add-hook 'enh-ruby-mode-hook 'zenspider/erm/flycheck)
+;; (remove-hook 'enh-ruby-mode-hook 'zenspider/erm/flycheck)
+
+(defun zenspider/magit/skip-tags ()
+  (add-to-list 'magit-refs-filter-alist '("^[v0-9]")))
+
+(add-hook 'magit-mode-hook 'zenspider/magit/skip-tags)
+;; (remove-hook 'magit-mode-hook 'zenspider/magit/skip-tags)
