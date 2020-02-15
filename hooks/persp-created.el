11c1fbf85b38d27dadecab448843f83341b2f2ab
Ryan Davis <ryand-ruby@zenspider.com>
2020-02-14 16:53:12 -0800

    Fixed shell pwd race condition when default perspective made [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12570]

--- hooks/persp-created.el
+++ hooks/persp-created.el
@@ -3,10 +3,12 @@
 (let* ((curr-name (persp-current-name))
        (maybe-dir (concat (f-canonical (f-join "~/Links" curr-name)) "/"))
        (proj--dir
-        (cond ((f-directory? maybe-dir) maybe-dir)
+        (cond ((equal "*" curr-name) (f-canonical "~/"))
+              ((f-directory? maybe-dir) maybe-dir)
               ((s-lowercase? curr-name) default-directory)
               (t (call-interactively
                   #'(lambda (path) (interactive "Glocation: ") path))))))
   (with-current-buffer (persp-scratch-buffer)
+    (message "creating perspective in %s" proj--dir)
     (setq default-directory proj--dir)
     (rwd-shell)))
