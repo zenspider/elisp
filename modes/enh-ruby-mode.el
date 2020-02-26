ee0ab28cb72855985414e8ffeadb2e305a0f6d64
Ryan Davis <ryand-ruby@zenspider.com>
2020-02-26 00:31:15 -0800

    don't treat Gemfile.lock as a ruby file [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12584]

--- modes/enh-ruby-mode.el
+++ modes/enh-ruby-mode.el
@@ -3,7 +3,7 @@
 (dolist (spec '(("\\.mab$"   . enh-ruby-mode)
                 ("\\.rb$"    . enh-ruby-mode)
                 ("Rakefile"  . enh-ruby-mode)
-                ("Gemfile"  . enh-ruby-mode)
+                ("Gemfile$"  . enh-ruby-mode)
                 ("\\.rake$"  . enh-ruby-mode)))
   (add-to-list 'auto-mode-alist spec))
 
