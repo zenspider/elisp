d9627fd4391386eb3e6954a702a80d576eddcdc6
Ryan Davis <ryand-ruby@zenspider.com>
2020-04-28 20:17:42 -0800

    changed uniquify to make it easier to see and tab complete what you want [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12621]

--- misc/rwd-uniquify.el
+++ misc/rwd-uniquify.el
@@ -4,8 +4,8 @@
 
   (setq
    uniquify-strip-common-suffix t
-   uniquify-separator           "/"
-   uniquify-buffer-name-style   'forward
+   uniquify-separator           "::"
+   uniquify-buffer-name-style   'post-forward
    uniquify-after-kill-buffer-p t      ; rename after killing uniquified
    uniquify-ignore-buffers-re   "^\\*" ; don't  muck  with    special buffers
    ))
