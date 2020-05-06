dd22963f213f880ba974e6f8d66073365f824e19
Ryan Davis <ryand-ruby@zenspider.com>
2020-05-06 09:29:03 -0800

    Added extra resolutions [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12635]

--- modes/dispwatch.el
+++ modes/dispwatch.el
@@ -1,7 +1,8 @@
 (defvar rwd/displays
-  `(("1440x900"  ,(* 1 80) ,(* 2 80) "Menlo" 14)
-    ("1792x1120" ,(* 1 80) ,(* 2 80) "Menlo" 16)
-    ("4000x1440" ,(* 2 60) ,(* 2 80) "Menlo" 16))
+  `(("1440x900"  ,(* 1 80) ,(* 2 80) "Fira Code" 14) ; normal 13" lappy
+    ("1792x1120" ,(* 2 80) ,(* 2 80) "Fira Code" 16) ; mpb 16" at regular
+    ("2048x1280" ,(* 2 60) ,(* 2 80) "Fira Code" 16) ; mbp 16" at densest
+    ("4000x1440" ,(* 2 60) ,(* 2 80) "Fira Code" 16)) ; chef monitor
   "Contains a list of display entries for emacs' dispwatch")
 
 (defun rwd/display-setup (h w f s)
