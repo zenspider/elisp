963760d47d13540773fc73351ce4675d46f6158b
Ryan Davis <ryand-ruby@zenspider.com>
2020-07-22 16:01:43 -0800

    some config for dumb-jump [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12770]

---
+++ modes/dumb-jump.el
@@ -0,0 +1,7 @@
+(dumb-jump-mode)
+
+(let ((map dumb-jump-mode-map))
+  (define-key map (kbd "C-c .") 'dumb-jump-go-other-window)
+  (define-key map (kbd "C-c ,") 'dumb-jump-go-prefer-external-other-window)
+  (define-key map (kbd "C-c *") 'dumb-jump-back)
+  (define-key map (kbd "C-c q") 'dumb-jump-quick-look))
