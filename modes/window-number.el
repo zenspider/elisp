531bc5c128f9c0e240b9463407ab2268df0aab8b
Ryan Davis <ryand-ruby@zenspider.com>
2020-11-21 18:14:29 -0800

    Removed window-number wrap message. It works [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12835]

--- modes/window-number.el
+++ modes/window-number.el
@@ -1,5 +1,4 @@
 (defun rwd/window-number-select (orig-fn number &rest args)
-  (message "wrapped window-number-select (%d)" number)
   (apply orig-fn (min (length (window-list nil nil)) number) args))
 
 (advice-add 'window-number-select :around 'rwd/window-number-select)
