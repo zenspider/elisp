b6889cb6d67c31df2dbfbc221df91ea06d602c76
Ryan Davis <ryand-ruby@zenspider.com>
2020-11-21 18:14:03 -0800

    Greatly simplified configuration. Created rwd/default-split-height and rwd/default-split-width. Made resolutions only have font size as a result. [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12834]

--- modes/dispwatch.el
+++ modes/dispwatch.el
@@ -6,42 +6,62 @@
   "Contains a list of display entries for emacs' dispwatch")
 
 (setq rwd/displays
-  `(("default"   ,(* 1 80) ,(* 2 80) "Fira Code" 14) ; fallback
+  `(("default"   14)
 
-    ("1440x900"  ,(* 1 80) ,(* 2 80) "Fira Code" 14) ; normal 13" lappy
+    ;; 13" MBP
+    ("1440x900"  14) ; *
+
+    ;; 13" MBP M1
+    ;; 1024x640 ?!?!
+    ("1280x800"  12)
+    ;; 1440x900        *
+    ("1680x1050" 16)
 
     ;; MBP 16"
-    ("1152x720"  ,(* 2 80) ,(* 2 80) "Fira Code" 10) ; largest  text
-    ("1344x840"  ,(* 2 80) ,(* 2 80) "Fira Code" 13) ; larger   text
-    ("1536x960"  ,(* 2 80) ,(* 2 80) "Fira Code" 15) ; medium   text
-    ("1792x1120" ,(* 2 80) ,(* 2 80) "Fira Code" 16) ; smaller  text (*)
-    ("2048x1280" ,(* 2 60) ,(* 2 80) "Fira Code" 18) ; smallest text
+    ("1152x720"  10)
+    ("1344x840"  13)
+    ("1536x960"  15)
+    ("1792x1120" 16) ; *
+    ("2048x1280" 18)
 
     ;; pairing MPB 16" w/ HP 27Z @ 60hz
-    ("1280x720"  ,(* 2 60) ,(* 2 80) "Fira Code"  9) ; largest  text
-    ("1920x1080" ,(* 2 60) ,(* 2 80) "Fira Code" 12) ; larger   text
-    ("2560x1440" ,(* 2 60) ,(* 2 80) "Fira Code" 16) ; medium   text
-    ("3200x1800" ,(* 2 60) ,(* 2 80) "Fira Code" 18) ; smaller  text
-    ("3840x2160" ,(* 2 60) ,(* 2 80) "Fira Code" 24) ; smallest text (*)
-    ("4000x1440" ,(* 2 60) ,(* 2 80) "Fira Code" 16) ; chef POS (dell?) monitor
+    ("1280x720"   9)
+    ("1920x1080" 12)
+    ("2560x1440" 16)
+    ("3200x1800" 18)
+    ("3840x2160" 24) ; *
 
     ;; 13" MBP paired with HP 27Z @ 60hz usbc
-    ;;1920x1080                                      ; largest  text (*)
-    ;;2560x1440                                      ; larger   text
-    ("3008x1692" ,(* 2 60) ,(* 2 80) "Fira Code" 18) ; medium   text
-    ("3360x1890" ,(* 2 60) ,(* 2 80) "Fira Code" 20) ; smaller  text
-    ;;3840x2160                                      ; smallest text
+    ;;1920x1080        *
+    ;;2560x1440
+    ("3008x1692" 18)
+    ("3360x1890" 20)
+    ;;3840x2160
+
+    ;; 13" MBP M1 paired with HP 27Z @ 60hz usbc
+
+    ("1504x846"  15)
+    ;;1920x1080
+    ;;2560x1440
+    ;;3008x1692        *
+    ;;3840x2160
     ))
 
 ;; (rwd/display-reset)
 ;; (rwd-split-smart)
 ;; (rwd/current-display)
 
-(defun rwd/display-setup (h w f s)
-  (setq split-height-threshold h
-        split-width-threshold w)
-  (rwd-set-mac-font f s)
-  (message "rwd/display-setup %s %s %s %s" h w f s))
+(defcustom rwd/default-font "Fira Code"
+  "Default font to use in all windows.")
+
+(defcustom rwd/default-split-height (* 2 60) "Used by rwd/display-setup")
+(defcustom rwd/default-split-width  (* 2 80) "Used by rwd/display-setup")
+
+(defun rwd/display-setup (s)
+  (setq split-height-threshold rwd/default-split-height
+        split-width-threshold  rwd/default-split-width)
+  (rwd-set-mac-font rwd/default-font s)
+  (message "rwd/display-setup %s %s %s %s" h w rwd/default-font s))
 
 (defun rwd/current-display ()
   (let* ((attr (frame-monitor-attributes))
@@ -65,7 +85,7 @@
       (message "rwd/display-change %s" disp)
       (unless args
         (find-variable 'rwd/displays)
-        (insert (format "%S\n" (list disp 'w 'h "Font" 16)))
+        (insert (format "%S\n" (list disp 'w 'h 16)))
         (message "Please extend rwd/displays with %S" disp)
         (setq args default))
       (apply 'rwd/display-setup (cdr args))
@@ -85,3 +105,5 @@
   "Reset and force dispwatch to trigger again."
   (interactive)
   (setq dispwatch-current-display nil))
+
+;; (rwd/display-reset)
