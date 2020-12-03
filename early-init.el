e6adac1c5da72d4df5df139f61feec37351d7325
Ryan Davis <ryand-ruby@zenspider.com>
2020-12-03 01:37:20 -0800

    Added early-init.el [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12857]

---
+++ early-init.el
@@ -0,0 +1,8 @@
+(setq garbage-collection-messages t          ; indicator of thrashing
+      gc-cons-threshold most-positive-fixnum ; 2^61 bytes
+      gc-cons-percentage 0.6                 ; blah
+      load-prefer-newer t                    ; ignore stale elc files
+      ;; TODO: package-enable-at-startup nil       ; check w/ package--initialized
+      ;; TODO: package-load-list '()
+      ;; TODO: package--init-file-ensured t
+      )
