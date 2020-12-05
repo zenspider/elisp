b56b889e061b105fb180451cb9c46638bcb4508b
Ryan Davis <ryand-ruby@zenspider.com>
2020-12-05 13:00:51 -0800

    switched to rg [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12876]

--- modes/ag.el
+++
@@ -1,5 +0,0 @@
-(require 'ag)
-(unless (equalp (symbol-function 'ag) 'ag-regexp) ; don't let this run twice
-  (message "rewiring ag")
-  (fset 'ag-literal (symbol-function 'ag))
-  (defalias 'ag 'ag-regexp))
