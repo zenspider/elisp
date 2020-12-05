5729254fca69854c0ba930b7471fbbf228855400
Ryan Davis <ryand-ruby@zenspider.com>
2020-12-05 02:15:02 -0800

    Refactored rwd-autoloads to be a little more digestable and report on time of each phase [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12867]

--- rwd-autoloads.el
+++ rwd-autoloads.el
@@ -1,34 +1,43 @@
+(eval-when-compile
+  (require 'rwd-load)
+  (require 'find-lisp))
+
+(require 'autoload)                     ; = ;;;###autoload
+
 (defun rwd-recompile-init ()
   (interactive)
   (message "recompiling %s" user-init-dir)
   (byte-recompile-directory (expand-file-name user-init-dir) 0))
 
-(setq generated-autoload-file "loaddefs.el") ;; HACK for emacs 24.1
+(setq generated-autoload-file (concat user-init-dir "loaddefs.el"))
 
-(autoload 'find-lisp-find-files          "find-lisp" nil t)
-(autoload 'find-lisp-find-files-internal "find-lisp" nil t)
+
+(defun rwd-autoloads-out-of-date-p (autoload-file)
+  (report-time 'rwd-autoloads-out-of-date-p
+    (or (not (file-exists-p autoload-file))
+        (catch 'newer
+          (dolist (file (find-lisp-find-files user-init-dir "\\.el$"))
+            (when (file-newer-than-file-p file autoload-file)
+              (message "NEWER! %s" file)
+              (throw 'newer file)))))))
+
+(defun rwd-generate-autoloads (autoload-file)
+  (report-time 'rwd-generate-autoloads
+    (let ((generated-autoload-file autoload-file)
+          (el-root-subdirs (find-lisp-find-files-internal
+                            user-init-dir
+                            'find-lisp-file-predicate-is-directory
+                            'find-lisp-default-directory-predicate)))
+      (apply 'update-directory-autoloads (cons user-init-dir el-root-subdirs))
+      (rwd-recompile-init)
+      (set-file-times autoload-file))))
 
 (defun rwd-autoloads ()
   "Regenerate the autoload definitions file if necessary and load it."
   (interactive)
-  (let* ((autoload-file (concat user-init-dir generated-autoload-file)))
-    (if (or (not (file-exists-p autoload-file))
-            (catch 'newer
-              (dolist (file (find-lisp-find-files user-init-dir "\\.el$"))
-                (if (file-newer-than-file-p file autoload-file)
-                    (throw 'newer file)))))
-        (let ((generated-autoload-file autoload-file)
-              (el-root-subdirs (find-lisp-find-files-internal
-                                user-init-dir
-                                'find-lisp-file-predicate-is-directory
-                                'find-lisp-default-directory-predicate)))
-          (apply 'update-directory-autoloads (cons user-init-dir el-root-subdirs))
-          (rwd-recompile-init)
-          (load autoload-file) ; helps rwd-recompile-init dependencies
-
-          ))
-    (message "loading autoloads")
-    (load autoload-file)
-    (message "done loading autoloads")))
+  (report-time 'rwd-autoloads
+    (when (rwd-autoloads-out-of-date-p generated-autoload-file)
+      (rwd-generate-autoloads generated-autoload-file))
+    (rwd-load generated-autoload-file)))
 
 (provide 'rwd-autoloads)
