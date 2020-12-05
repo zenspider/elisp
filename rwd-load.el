9f20574c2747242807ddcac01296d0f6005ba5b9
Ryan Davis <ryand-ruby@zenspider.com>
2020-12-05 02:12:39 -0800

    Added report-time and when-idle to rwd-load.el [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12865]

--- rwd-load.el
+++ rwd-load.el
@@ -1,6 +1,16 @@
+(defmacro report-time (name &rest body)
+  (declare (indent defun))
+  `(let ((t1 (current-time))
+         (result (progn ,@body)))
+     (message "DONE: %-50S in %.2f sec" ,name (float-time (time-since t1)))
+     result))
+
+(defmacro when-idle (delay &rest body)
+  (declare (indent defun))
+  `(run-with-idle-timer ,delay nil (lambda () ,@body)))
+
 (defun rwd-load (&rest args)
-  (let ((t1 (current-time)))
-    (apply 'load args)
-    (message "DONE: %-50S in %.2f sec" `(rwd-load ,@args) (float-time (time-since t1)))))
+  (report-time `(rwd-load ,@args)
+    (apply 'load args)))
 
 (provide 'rwd-load)
