1698bee288b07ca0901345c5e6c409a9bf27c8e7
Ryan Davis <ryand-ruby@zenspider.com>
2020-12-06 02:49:01 -0800

    hook tramp to only set stuff when tramp is loaded/invoked [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12881]

--- modes/tramp.el
+++ modes/tramp.el
@@ -1,5 +1,8 @@
-;; (setq tramp-verbose 9)
-(require 'tramp)
-(require 'tramp-sh)
-(setq tramp-ssh-controlmaster-options
-      "-o ControlPath=~/.ssh/tramp.%%C -o ControlMaster=auto -o ControlPersist=no")
+(eval-when-compile
+  (require 'tramp)
+  (require 'tramp-sh))
+
+(eval-after-load 'tramp
+  ;; (setq tramp-verbose 9)
+  (setq tramp-ssh-controlmaster-options
+        "-o ControlPath=~/.ssh/tramp.%%C -o ControlMaster=auto -o ControlPersist=no"))
