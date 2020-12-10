71842033b645cec1b30cec472bf69709e464e22f
Ryan Davis <ryand-ruby@zenspider.com>
2020-12-10 02:29:03 -0800

    Added byte-recompile checking to rwd-autoloads [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12905]

--- rwd-autoloads.el
+++ rwd-autoloads.el
@@ -5,23 +5,27 @@
 
 (defun rwd-recompile-init ()
   (interactive)
-  (message "recompiling %s" user-init-dir)
-  (byte-recompile-directory (expand-file-name user-init-dir) 0))
+  (report-time 'rwd-recompile-init
+    (byte-recompile-directory (expand-file-name user-init-dir) 0)))
 
-(setq generated-autoload-file (concat user-init-dir "loaddefs.el"))
+(setq autoload-file (concat user-init-dir "loaddefs.el"))
 
 (require 'find-lisp)
 
-(defun rwd-autoloads-out-of-date-p (autoload-file)
+(defun rwd-autoloads-out-of-date-p ()
   (report-time 'rwd-autoloads-out-of-date-p
     (or (not (file-exists-p autoload-file))
         (catch 'newer
           (dolist (file (find-lisp-find-files user-init-dir "\\.el$"))
-            (when (file-newer-than-file-p file autoload-file)
-              (message "NEWER! %s" file)
-              (throw 'newer file)))))))
+            (unless (equal file autoload-file)
+              (let ((elc-file (byte-compile-dest-file file)))
+                (when (or (file-newer-than-file-p file autoload-file)
+                          (not (file-exists-p elc-file))
+                          (file-newer-than-file-p file elc-file))
+                  (message "NEWER! %s" file)
+                  (throw 'newer file)))))))))
 
-(defun rwd-generate-autoloads (autoload-file)
+(defun rwd-generate-autoloads ()
   (report-time 'rwd-generate-autoloads
     (let ((generated-autoload-file autoload-file)
           (el-root-subdirs (find-lisp-find-files-internal
@@ -36,8 +40,8 @@
   "Regenerate the autoload definitions file if necessary and load it."
   (interactive)
   (report-time 'rwd-autoloads
-    (when (rwd-autoloads-out-of-date-p generated-autoload-file)
-      (rwd-generate-autoloads generated-autoload-file))
-    (rwd-load generated-autoload-file)))
+    (when (rwd-autoloads-out-of-date-p)
+      (rwd-generate-autoloads))
+    (rwd-load autoload-file)))
 
 (provide 'rwd-autoloads)
