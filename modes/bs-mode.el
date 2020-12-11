3c1bf23575bb913123e3afdc2624351b5fa4e76c
Ryan Davis <ryand-ruby@zenspider.com>
2020-12-11 15:11:41 -0800

    Wrap bs-mode config to only set up persp config if in window-system [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12909]

--- modes/bs-mode.el
+++ modes/bs-mode.el
@@ -7,18 +7,19 @@
   (with-current-buffer (or buf (current-buffer))
     major-mode))
 
-(defun rwd/persp/same-mode (current-buf)
-  (let* ((orig-mode    (rwd/buffer-major-mode bs--buffer-coming-from))
-         (current-mode (rwd/buffer-major-mode current-buf)))
-    (and (persp-is-current-buffer current-buf)
-         (equal orig-mode current-mode))))
+(when window-system
+ (defun rwd/persp/same-mode (current-buf)
+   (let* ((orig-mode    (rwd/buffer-major-mode bs--buffer-coming-from))
+          (current-mode (rwd/buffer-major-mode current-buf)))
+     (and (persp-is-current-buffer current-buf)
+          (equal orig-mode current-mode))))
 
-(add-to-list 'bs-configurations '("persp"
-                                  nil nil
-                                  "^\\*" persp-buffer-filter
-                                  bs-sort-buffer-interns-are-last))
+ (add-to-list 'bs-configurations '("persp"
+                                   nil nil
+                                   "^\\*" persp-buffer-filter
+                                   bs-sort-buffer-interns-are-last))
 
-(add-to-list 'bs-configurations '("current-mode"
-                                  nil rwd/persp/same-mode
-                                  "." nil
-                                  nil))
+ (add-to-list 'bs-configurations '("current-mode"
+                                   nil rwd/persp/same-mode
+                                   "." nil
+                                   nil)))
