b01480c749f67f347adbd5f8148847d268c0718f
Ryan Davis <ryand-ruby@zenspider.com>
2020-12-12 01:53:20 -0800

    Fixed loading / use of rg. Added search-by-word menu cmd [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12913]

--- modes/rg.el
+++ modes/rg.el
@@ -1,2 +1,8 @@
 (autoload 'rg-menu "rg-menu" "doco" t)
 (global-set-key (kbd "C-c r") #'rg-menu)
+
+(with-eval-after-load 'rg-menu
+  (require 'rg)
+  (rg-define-search rg-search-for-word
+    :flags ("--word-regexp")
+    :menu ("Search" "w" "Word")))
