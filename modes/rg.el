1b8e0924e8b3db12e8a89465c900288b9aca7543
Ryan Davis <ryand-ruby@zenspider.com>
2020-12-12 13:08:44 -0800

    Fixed up rg by filing an issue upstream [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12915]

--- modes/rg.el
+++ modes/rg.el
@@ -2,7 +2,7 @@
 (global-set-key (kbd "C-c r") #'rg-menu)
 
 (with-eval-after-load 'rg-menu
-  (require 'rg)
+  (declare-function rg-search-for-word "modes/rg")
   (rg-define-search rg-search-for-word
     :flags ("--word-regexp")
     :menu ("Search" "w" "Word")))
