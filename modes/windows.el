520ce15d4403a194678ad6523be3cf7fc00f0488
Ryan Davis <ryand-ruby@zenspider.com>
2020-12-20 03:44:03 -0800

    speed up modes/windows by deferring as much as possible [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12930]

--- modes/windows.el
+++ modes/windows.el
@@ -1,4 +1,7 @@
-(windmove-default-keybindings)
-(winner-mode 1)
-(require 'window-number)                ; TODO: maybe idle
-(window-number-meta-mode 1)
+(when-idle rwd-idle-time
+  (windmove-default-keybindings)
+  (winner-mode 1)
+  (rwd-require 'window-number))
+
+(with-eval-after-load 'window-number
+  (window-number-meta-mode 1))
