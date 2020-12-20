5b4f0ac8cfaa77d1dc7d4bd186848049a77d1e60
Ryan Davis <ryand-ruby@zenspider.com>
2020-12-20 03:43:06 -0800

    remove seeing-is-believing [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12929]

--- modes/enh-ruby-mode.el
+++ modes/enh-ruby-mode.el
@@ -1,3 +1,5 @@
+(add-to-list 'load-path (expand-file-name "~/Work/git/zenspider/enhanced-ruby-mode"))
+
 (autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for ruby files" t)
 
 (dolist (spec '(("\\.mab$"   . enh-ruby-mode)
@@ -8,12 +10,3 @@
   (add-to-list 'auto-mode-alist spec))
 
 (add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
-
-(add-to-list 'load-path (expand-file-name "~/Work/git/zenspider/enhanced-ruby-mode"))
-(add-to-list 'load-path (expand-file-name "~/Work/git/jcinnamond/seeing-is-believing"))
-
-(when (require 'seeing-is-believing nil t)
-  (defun zenspider/sib/erm+refresh ()
-    (erm-reset-buffer))
-  (add-hook 'seeing-is-believing-after-run-hooks
-	    'zenspider/sib/erm+refresh))
