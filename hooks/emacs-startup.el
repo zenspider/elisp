8badc7140eff7310be74139b16eae4ec1f59ee94
Ryan Davis <ryand-ruby@zenspider.com>
2020-12-20 03:38:03 -0800

    don't load persp-mode from hooks/emacs-startup now that it loads all modes directly again [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12926]

--- hooks/emacs-startup.el
+++ hooks/emacs-startup.el
@@ -1,7 +1,4 @@
-(rwd-load-modes)                        ; delayed loading
-
-(rwd-load "./modes/persp-mode" nil t)   ; force early load
+(rwd-load-modes)
 
 (when (and normal-gui-startup (not (getenv "INSIDE_EMACS")))
-  (message "Starting SERVER")
   (server-start))
