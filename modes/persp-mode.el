7057da0d973fbb2e350b591feef90e68385afa45
Ryan Davis <ryand-ruby@zenspider.com>
2020-12-20 03:46:31 -0800

    Speed up persp-mode as much as possibly by deferring... also just don't activate unless normal-gui-startup [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12933]

--- modes/persp-mode.el
+++ modes/persp-mode.el
@@ -1,17 +1,17 @@
+(let ((persp-path (expand-file-name "~/Work/git/nex3/perspective-el")))
+  (when (file-directory-p persp-path)
+    (add-to-list 'load-path persp-path)))
 
-(setq persp-path (expand-file-name "~/Work/git/nex3/perspective-el"))
+(when normal-gui-startup
+  (when-idle rwd-idle-time
+    (rwd-require 'perspective)))
 
-(when (file-directory-p persp-path)
-  (add-to-list 'load-path persp-path))
+(with-eval-after-load 'perspective
+  (define-key perspective-map (kbd "d") 'persp-kill)
+  (define-key perspective-map (kbd "RET") 'persp-switch-last)
 
-(require 'perspective)
+  (unless window-system         ; restore C-z z and C-z C-z to suspend
+    (define-key perspective-map (kbd "z")   'suspend-frame)
+    (define-key perspective-map (kbd "C-z") 'suspend-frame))
 
-(define-key perspective-map (kbd "d") 'persp-kill)
-(define-key perspective-map (kbd "RET") 'persp-switch-last)
-
-(unless window-system                   ; restore C-z z and C-z C-z to suspend
-  (define-key perspective-map (kbd "z")   'suspend-frame)
-  (define-key perspective-map (kbd "C-z") 'suspend-frame))
-
-(when (and window-system (not persp-mode))
   (persp-mode))
