36b8b60acb22ad1ed37a25410bf7c733e31854ee
Ryan Davis <ryand-ruby@zenspider.com>
2020-12-20 13:21:32 -0800

    Report time for autohooks [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12936]

--- rwd-autohooks.el
+++ rwd-autohooks.el
@@ -2,21 +2,21 @@
 
 (defun rwd-autohooks ()
   (interactive)
-  (dolist (path (directory-files (concat user-init-dir "hooks") t ".*el$"))
-    (let* ((mode       (file-name-nondirectory (file-name-sans-extension path)))
-           (hook-name  (intern (concat mode "-hook")))
-           (raw-name   (concat "rwd-" mode "-hook"))
-           (defun-name (intern raw-name))
-           (lisp       (read-file-to-string path))
-           (debug      nil))
-      (when (functionp defun-name)
-        (remove-hook hook-name defun-name))
-      (eval (read (concat "(defun " raw-name " () "
-                          (if debug
-                              (format "%S" `(message "running %s" ,raw-name))
-                            "")
-                          lisp ")")))
-      (add-hook hook-name defun-name)))
-  (message "DONE: rwd-autohooks"))
+  (report-time 'rwd-autohooks
+    (dolist (path (directory-files (concat user-init-dir "hooks") t ".*el$"))
+      (let* ((mode       (file-name-nondirectory (file-name-sans-extension path)))
+             (hook-name  (intern (concat mode "-hook")))
+             (raw-name   (concat "rwd-" mode "-hook"))
+             (defun-name (intern raw-name))
+             (lisp       (read-file-to-string path))
+             (debug      nil))
+        (when (functionp defun-name)
+          (remove-hook hook-name defun-name))
+        (eval (read (concat "(defun " raw-name " () "
+                            (if debug
+                                (format "%S" `(message "running %s" ,raw-name))
+                              "")
+                            lisp ")")))
+        (add-hook hook-name defun-name)))))
 
 (provide 'rwd-autohooks)
