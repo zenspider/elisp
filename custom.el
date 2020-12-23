78cd245c6f0d5bdb3396074a027a83f3c3fa5da1
Ryan Davis <ryand-ruby@zenspider.com>
2020-12-22 16:04:06 -0800

    Make ielm clearable, and then bind `C-c e` to my comint-erase fn.
    [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 12951]

--- custom.el
+++ custom.el
@@ -84,6 +84,7 @@
  '(hippie-expand-try-functions-list
    '(try-expand-all-abbrevs try-expand-dabbrev-visible try-expand-dabbrev try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill rwd-try-expand-tag try-complete-lisp-symbol-partially try-complete-lisp-symbol try-complete-file-name-partially try-complete-file-name try-expand-list))
  '(history-length 1000)
+ '(ielm-prompt-read-only nil)
  '(imenu-max-items 50)
  '(imenu-sort-function 'imenu--sort-by-name)
  '(indent-tabs-mode nil)
---
+++ modes/ielm-mode.el
@@ -0,0 +1,2 @@
+(with-eval-after-load 'ielm-mode
+  (define-key ielm-map (kbd "C-c e") 'rwd-shell-clear))
