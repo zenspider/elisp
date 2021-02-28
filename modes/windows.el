497c7b4e6e00423bb6a01757b24f317d90101ccd
Ryan Davis <ryand-ruby@zenspider.com>
2021-02-27 17:29:08 -0800

    Moved the rest into modes/windows.el
    [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 13039]

--- modes/windows.el
+++ modes/windows.el
@@ -39,6 +39,37 @@ the ACTIONS alist."
                                        (column . ,nth))
                'append))
 
+(defun interactive-split-current-window ()
+  "Interactively split the current window, either horizontally or
+vertically. The rule of thumb is that this function favors a
+horizontal split, unless it would result in windows narrower than
+the current fill-column."
+  (interactive)
+  (if (> (window-width) (* 2 fill-column))
+      (split-window-horizontally)
+    (split-window-vertically)))
+
+(defun rwd/window-columns ()
+  (/ (frame-width) 80))
+
+;; TODO: keep poking at using split-width-threshold and friends on the
+;; large monitor and decide if I need to override
+;; split-window-sensibly to favor vertical splits. This is a start:
+
+;; https://emacs.stackexchange.com/questions/20492/how-can-i-get-a-sensible-split-window-policy
+(defun rwd/split-window-sensibly (&optional window)
+    "replacement `split-window-sensibly' function which prefers vertical splits"
+    (interactive)
+    (let ((window (or window (selected-window))))
+        (or (and (window-splittable-p window t)
+                 (with-selected-window window
+                     (split-window-right)))
+            (and (window-splittable-p window)
+                 (with-selected-window window
+                     (split-window-below))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (setq display-buffer-alist nil)
 
 ;; (rwd/nth-column-for-matching-buffers  0 "^shell-")
@@ -53,11 +84,6 @@ the ACTIONS alist."
 (rwd/nth-column-for-matching-buffers -1 "^.P4 diff")
 (rwd/nth-column-for-matching-buffers -1 "\\*info\\|\\*help")
 (rwd/nth-column-for-matching-buffers -1 "^magit:")
-
-;; ; truncate-lines: t
-;;                                                                                                 1                                                                                                   2
-;;       1         2         3         4         5         6         7         8         9         0         1         2         3         4         5         6         7         8         9         0
-;;34567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
