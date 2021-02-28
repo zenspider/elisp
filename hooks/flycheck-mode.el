065cbd61e0ddf7a2bd5adaba34083b889abaaa2f
Ryan Davis <ryand-ruby@zenspider.com>
2021-02-27 17:31:03 -0800

    Following instructions [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 13041]

--- hooks/flycheck-mode.el
+++ hooks/flycheck-mode.el
@@ -2,5 +2,4 @@
                                      (display-buffer-reuse-window
                                       display-buffer-below-selected)
                                      (window-height   . 0.33)))
-(flycheck-package-setup)
 (flycheck-color-mode-line-mode)
