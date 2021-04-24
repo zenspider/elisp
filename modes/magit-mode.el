5bca99aa46e87feb7eca39b3e176c4771ef0785f
Ryan Davis <ryand-ruby@zenspider.com>
2021-04-24 12:35:38 -0800

    fixed loading problem for magit path fixer [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 13078]

--- modes/magit-mode.el
+++ modes/magit-mode.el
@@ -1,12 +1,14 @@
+(require 'magit)
+
 (with-eval-after-load 'transient
   (transient-bind-q-to-quit))
 
 ;; M1 macs install into /opt/homebrew/bin:
 
-(unless (f-executable-p magit-git-executable)
+(unless (file-executable-p magit-git-executable)
   (setq magit-git-executable "/opt/homebrew/bin/git"))
 
-(unless (f-executable-p with-editor-emacsclient-executable)
+(unless (file-executable-p with-editor-emacsclient-executable)
   (setq with-editor-emacsclient-executable "/opt/homebrew/bin/emacsclient"))
 
 ;; from http://www.ogre.com/node/447
