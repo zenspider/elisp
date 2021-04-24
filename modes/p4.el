21b1e0a5cfbe971ad25a85517aa4e087c213c5a2
Ryan Davis <ryand-ruby@zenspider.com>
2021-04-24 12:34:58 -0800

    Fixed pathing problem for M1 macs [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 13077]

--- modes/p4.el
+++ modes/p4.el
@@ -1,10 +1,12 @@
 (with-eval-after-load 'p4
   (p4-set-p4-config ".p4config")
 
-  (let* ((paths '("/usr/local/bin/p4" "/opt/perforce/bin/p4"))
+  (let* ((paths '("/usr/local/bin/p4" "/opt/perforce/bin/p4" "/opt/homebrew/bin/p4"))
          (paths (mapcar   #'expand-file-name paths))
          (found (seq-find #'file-exists-p    paths)))
-    (p4-set-p4-executable found))
+    (if found
+        (p4-set-p4-executable found)
+      (message "P4 executable not found!")))
 
   (define-key p4-prefix-map (kbd "A") 'p4-diff-all-opened))
 
