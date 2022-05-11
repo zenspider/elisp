7725c08f00a463ba7210efcb759c934223c85b00
Ryan Davis <ryand-ruby@zenspider.com>
2022-05-11 10:08:11 -0800

    Added patch to fix ffap in emacs 28 to always use ffap-file-finder. [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 13374]

---
+++ modes/ffap.el
@@ -0,0 +1,30 @@
+;;; stolen from https://github.com/sigma/el-x/blob/master/lisp/dflet.el
+
+(require 'cl-lib)
+
+(defmacro dflet (bindings &rest body)
+  "Make temporary overriding function definitions.
+This is an analogue of a dynamically scoped `let' that operates on the function
+cell of FUNCs rather than their value cell.
+
+\(fn ((FUNC ARGLIST BODY...) ...) FORM...)"
+  (declare (indent 1) (debug cl-flet))
+  `(cl-letf ,(mapcar
+              (lambda (x)
+                (list
+                 (list 'symbol-function (list 'quote (car x)))
+                 (cons 'lambda (cons (cadr x) (cddr x)))))
+              bindings)
+     ,@body))
+
+;;; end of stolen code
+
+(require 'ffap)
+
+(defun rwd/ffap-like-emacs27 (orig-fun &rest args)
+  ;; put simply, replace that last find-file with ffap-file-finder
+  (dflet ((find-file (filename &optional wildcards)
+             (funcall ffap-file-finder (expand-file-name filename))))
+    (apply orig-fun args)))
+
+(advice-add 'find-file-at-point :around #'rwd/ffap-like-emacs27)
