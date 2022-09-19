742f0938d98fdcbd8cc1369b0980eca5c4982857
Ryan Davis <ryand-ruby@zenspider.com>
2022-09-19 10:02:42 -0800

    wired up xref-find/pop to the usual keys [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 13543]

--- modes/dumb-jump.el
+++ modes/dumb-jump.el
@@ -1,7 +1,11 @@
 (with-eval-after-load 'dumb-jump
   (add-hook 'xref-backend-functions #'dumb-jump-xref-activate)
+
+  ;; (setq xref-show-definitions-function #'xref-show-definitions-completing-read)
+  ;; (setq xref-show-definitions-function 'xref-show-definitions-buffer)
+
   (let ((map dumb-jump-mode-map))
-    (define-key map (kbd "C-c .") 'dumb-jump-go-other-window)
-    (define-key map (kbd "C-c ,") 'dumb-jump-go-prefer-external-other-window)
+    (define-key map (kbd "C-c .") 'xref-find-definitions-other-window)
+    (define-key map (kbd "C-c ,") 'xref-pop-marker-stack)
     (define-key map (kbd "C-c *") 'dumb-jump-back)
     (define-key map (kbd "C-c q") 'dumb-jump-quick-look)))
