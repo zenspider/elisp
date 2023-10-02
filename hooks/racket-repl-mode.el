1902145bb829343f37a66134814d5d2609bd369e
Ryan Davis <ryand-ruby@zenspider.com>
2023-10-02 10:06:10 -0800

    workaround for coterm + racket-repl-mode blowup [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 13930]

--- hooks/racket-repl-mode.el
+++ hooks/racket-repl-mode.el
@@ -1,2 +1,7 @@
 (paredit-mode 1)
 (racket-unicode-input-method-enable)
+
+(coterm-mode -1)
+;; HACK: coterm-mode -1 doesn't fully disconnect
+(remove-function (process-filter (get-buffer-process (current-buffer)))
+                 #'coterm--t-emulate-terminal)
