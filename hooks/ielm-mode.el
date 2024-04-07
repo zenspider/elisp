9623e394640e090f5b9d31ec8205d455070ac595
Ryan Davis <ryand-ruby@zenspider.com>
2024-04-07 14:18:31 -0800

    Fixed up ielm by using rwd-keymap-holepunch, added eldoc-mode and persistent history. [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 14100]

--- hooks/ielm-mode.el
+++ hooks/ielm-mode.el
@@ -1,7 +1,11 @@
 (define-key ielm-map (kbd "C-c e") 'rwd-shell-clear)
+(rwd-keymap-holepunch 'paredit-mode "RET" "C-j") ; this punches through paredit
 
-(turn-on-elisp-slime-nav-mode)
+;; some code adapted from https://www.n16f.net/blog/making-ielm-more-comfortable
+
+(elisp-slime-nav-mode)
 (paredit-mode +1)
+(eldoc-mode)
 
 (require 'eval-in-repl)
 (require 'eval-in-repl-ielm)
@@ -12,3 +16,12 @@
 (define-key emacs-lisp-mode-map       (kbd "<C-return>") 'eir-eval-in-ielm)
 (define-key lisp-interaction-mode-map (kbd "<C-return>") 'eir-eval-in-ielm)
 (define-key Info-mode-map             (kbd "<C-return>") 'eir-eval-in-ielm)
+
+(let ((path (expand-file-name "ielm/history" user-emacs-directory)))
+  (make-directory (file-name-directory path) t)
+  (setq-local comint-input-ring-file-name path))
+(setq-local comint-input-ring-size 10000)
+(setq-local comint-input-ignoredups t)
+(comint-read-input-ring)
+
+(add-hook 'comint-input-filter-functions #'rwd/ielm/save-history)
