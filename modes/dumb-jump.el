596e07a4a02f8f03d6cdcdc2744d155a6752a57f
Ryan Davis <ryand-ruby@zenspider.com>
2024-04-07 14:20:40 -0800

    Cleaned up modes/dumb-jump.el now that it is working again. [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 14101]

--- modes/dumb-jump.el
+++ modes/dumb-jump.el
@@ -1,11 +1,2 @@
 (when-idle rwd-idle-time
-  (add-hook 'xref-backend-functions #'dumb-jump-xref-activate)
-
-  ;; (setq xref-show-definitions-function #'xref-show-definitions-completing-read)
-  ;; (setq xref-show-definitions-function 'xref-show-definitions-buffer)
-
-  (let ((map dumb-jump-mode-map))
-    (define-key map (kbd "C-c .") 'xref-find-definitions-other-window)
-    (define-key map (kbd "C-c ,") 'xref-pop-marker-stack)
-    (define-key map (kbd "C-c *") 'dumb-jump-back)
-    (define-key map (kbd "C-c q") 'dumb-jump-quick-look)))
+  (add-hook 'xref-backend-functions #'dumb-jump-xref-activate))
