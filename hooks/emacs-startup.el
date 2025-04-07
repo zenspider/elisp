84455815d9545830d763437097669ac7f8f4bbdc
Ryan Davis <ryand-ruby@zenspider.com>
2025-04-07 15:03:30 -0800

    Added tweak to auto-mode-interpreter-regexp to allow for /bin/env -S [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 14468]

--- hooks/emacs-startup.el
+++ hooks/emacs-startup.el
@@ -8,3 +8,30 @@
 
   (setq gc-cons-percentage 0.2
         gc-cons-threshold 1600000))
+
+;; original
+;; "#![ \t]?\\([^ \t\n]*/bin/env[ \t]\\)?\\([^ \t\n]+\\)"
+
+;; original, as rx
+;; (rx (seq "#!"
+;;          (opt (any "\t "))
+;;          (opt (group
+;;                (zero-or-more (not (any "\t\n ")))
+;;                "/bin/env"
+;;                (any "\t ")))
+;;          (group (one-or-more (not (any "\t\n "))))))
+
+;; mine:
+(setq auto-mode-interpreter-regexp
+      (rx (seq "#!"
+               (opt (any "\t "))
+               (opt (group
+                     (zero-or-more (not (any "\t \n")))
+                     "/bin/env"
+                     (any "\t ")
+                     (opt "-S" (+ (any "\t "))) ; ryand: added optional -S
+                     ))
+               (group (one-or-more (not (any "\t \n")))))))
+
+;; mine, back to regexp:
+;; "#![\t ]?\\([^\t\n ]*/bin/env[\t ]\\(?:-S[\t ]+\\)?\\)?\\([^\t\n ]+\\)"
