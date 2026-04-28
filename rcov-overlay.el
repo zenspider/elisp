136dc6ca75946bbb3a29f60027a290634d5c1b33
Ryan Davis <ryand-ruby@zenspider.com>
2026-04-28 14:46:54 -0800

    Overhaul rcov-overlay (to be renamed). Fix namespacing. Add coverage-summary. [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 15100]

--- rcov-overlay.el
+++ rcov-overlay.el
@@ -1,12 +1,12 @@
-;;; rcov-overlay.el --- Colorize untested ruby code
+;;; rcov-overlay.el --- Colorize untested ruby code  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2008-2022 by Ryan Davis
+;; Copyright (C) 2008-2026 by Ryan Davis
 
 ;; Author: Ryan Davis <ryand-ruby@zenspider.com>
 ;; URL: https://github.org/zenspider/elisp
 ;; Keywords: tools, languages
-;; Version: 1.3
-;; Package-Requires: ((a "1.0") (cl-lib "0.5") (emacs "24.1"))
+;; Version: 2.0
+;; Package-Requires: ((a "1.0") (json "1.5") (dash) (cl-lib "0.5") (emacs "29"))
 
 ;;; Posted using:
 ;; (emacswiki-post "rcov-overlay.el")
@@ -37,16 +37,13 @@
 ;;; Commentary:
 
 ;; This package provides the ability to highlight untested code
-;; according to rcov by using rcov's coverage.info serialized
-;; data. Also provided are two rake tasks to help generate the needed
-;; data.
+;; according to rcov by using simplecov's serialized data. Also
+;; provided are two rake tasks to help generate the needed data.
 
-;; See function `show-coverage'.
+;; This library used to be tooled to work with rcov, but that library
+;; has died.
 
-;; The function `overlay-current-buffer-with-command` is actually
-;; quite flexible as it will execute an external command that returns
-;; json data specifying regions and colors. It could be used for all
-;; sorts of mischief.
+;; See function `coverage-update' and `coverage-clear'.
 
 ;; Special thanks to Phil Hagelberg for writing augment, a much more
 ;; complete package for doing similar things (but still alpha enough
@@ -54,6 +51,7 @@
 
 ;;; History:
 
+;; 2.0 2026-04-19 Overhaul for simplecov, added summary, normalize naming.
 ;; 1.3 2022-10-17 Updated for modern emacs & simplecov, requires the 'a package.
 ;; 1.2 2009-10-21 Added customizable overlay background color.
 ;; 1.1 2008-12-01 Added find-project-dir to fix path generation issues.
@@ -66,79 +64,116 @@
 (require 'a)                            ; from melpa
 (require 'dash)
 
-(defcustom rcov-overlay-fg-color
+(defcustom coverage-fg-color
   "#ffcccc"
   "The default background color."
-  :group 'rcov-overlay
+  :group 'coverage
   :type 'color)
 
 ;; ruby-mode-map
 
 ;;;###autoload
-(defun show-coverage ()
+(defun coverage-update ()
   (interactive)
   (save-excursion
     (goto-char (point-min))
-    (remove-overlays)
-    (->> (buffer-name)
-         (coverage-for-buffer)
-         (coverage-to-lines)
-         (lines-to-regions)
-         (make-overlays-for-regions))))
+    (coverage-clear)
+    (->> (current-buffer)
+         (coverage--for-buffer)
+         (coverage--to-lines)
+         (coverage--lines-to-regions)
+         (coverage--regions-to-overlays))))
+
+(define-obsolete-function-alias 'show-coverage 'coverage-update "2026-04-15")
 
 ;;;###autoload
-(defun rcov-clear ()
+(defun coverage-clear ()
   (interactive)
   (remove-overlays))
 
+;;;###autoload
+(defun coverage-summary ()
+  (interactive)
+  (let* ((buffer   (current-buffer))
+         (cov-path (coverage--find-file-for-buffer buffer))
+         (coverage (coverage--read-json cov-path))
+         (top-key  (caar coverage))
+         (paths    (mapcar #'car (a-get* coverage top-key "coverage"))) ; FIX
+         (projpath (f-dirname (f-dirname cov-path))))
+    (with-output-to-temp-buffer "*Coverage Summary*"
+      (setq default-directory projpath)
+
+      (with-current-buffer standard-output
+        (button-mode)
+        (insert "Test Coverage Summary:\n\n")
+        (insert (format "%-40s %6s%% %4s %4s %4s %4s %6s\n" "Path" "cov" "line" "relv" "cov" "miss" "avghit"))
+        (mapc (lambda (path)
+                (let* ((data (a-get* coverage top-key "coverage" path "lines"))
+                       (args (coverage--summary-data data)))
+                  (insert
+                   (apply #'format
+                          "%-40s %6.2f%% %4d %4d %4d %4d %6.2f\n"
+                          (buttonize (f-relative path projpath) #'find-file path)
+                          args))))
+              (sort paths))
+        (pop-to-buffer "*Coverage Summary*")))))
+
 ;;; Utilities:
 
-(defun find-coverage-file-for-buffer (buffer-name)
- (with-current-buffer buffer-name
-   (let* ((cov-file "coverage/.resultset.json")
-          (base-dir (find-project-dir cov-file))
-          (cov-path (concat base-dir cov-file)))
-     cov-path)))
+(defun coverage--find-project-dir (file &optional dir)
+  (or dir (setq dir default-directory))
+  (if (file-exists-p (concat dir file))
+      dir
+    (if (equal dir "/")
+        nil
+      (coverage--find-project-dir file (expand-file-name (concat dir "../"))))))
 
-(defun coverage-read-json (cov-path)
+(defun coverage--find-file-for-buffer (buffer-name)
+  (with-current-buffer buffer-name
+    (let* ((cov-file "coverage/.resultset.json")
+           (base-dir (coverage--find-project-dir cov-file))
+           (cov-path (concat base-dir cov-file)))
+      cov-path)))
+
+(defun coverage--read-json (cov-path)
   (let ((json-array-type 'list)
         (json-key-type   'string))
     (json-read-file cov-path)))
 
-(defun coverage-for-buffer (buffer-name)
-  (let* ((buffer (get-buffer buffer-name))
+(defun coverage--for-buffer (buffer-name)
+  (let* ((buffer      (get-buffer buffer-name))
          (source-path (buffer-file-name buffer))
-         (cov-path (find-coverage-file-for-buffer buffer-name))
-         (coverage (coverage-read-json cov-path)))
-    (if (a-get* coverage "Minitest")
-        (a-get* coverage "Minitest" "coverage" source-path "lines")
-      (a-get* coverage "RSpec" "coverage" source-path "lines"))))
+         (cov-path    (coverage--find-file-for-buffer buffer-name))
+         (coverage    (coverage--read-json cov-path))
+         (top-key     (caar coverage)))
+    (a-get* coverage top-key "coverage" source-path "lines")))
 
-(defun coverage-to-lines (coverage)
+(defun coverage--summary-data (data)
+  (let* ((size    (length data))
+         (counts  (seq-keep  #'identity data))
+         (relv    (length counts))
+         (cov     (seq-count #'plusp counts))
+         (miss    (seq-count #'zerop counts))
+         (sum     (apply #'+ counts)))
+    (list (/ (* 100.0 cov) relv) size relv cov miss (/ (float sum) relv))))
+
+(defun coverage--to-lines (coverage)
   (->> coverage
        (-map-indexed #'cons)
        (--filter (eq 0 (cdr it)))
        (-map #'car)
        (-map #'1+)))
 
-(defun lines-to-regions (lines)
-  (--map (cons (point-at-bol it)
-               (point-at-eol it))
+(defun coverage--lines-to-regions (lines)
+  (--map (cons (line-beginning-position it)
+               (line-end-position it))
          lines))
 
-(defun make-overlays-for-regions (regions)
+(defun coverage--regions-to-overlays (regions)
   (--each regions
     (overlay-put (make-overlay (car it) (cdr it))
                  'face (cons 'background-color "#ffcccc"))))
 
-(defun find-project-dir (file &optional dir)
-  (or dir (setq dir default-directory))
-  (if (file-exists-p (concat dir file))
-      dir
-    (if (equal dir "/")
-        nil
-      (find-project-dir file (expand-file-name (concat dir "../"))))))
-
 (provide 'rcov-overlay)
 
-;;; rcov-overlay.el ends here
+;;; coverage.el ends here
