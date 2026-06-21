2eeb70e3de951994d7a24430a14632bac87529d5
Ryan Davis <ryand-ruby@zenspider.com>
2026-06-21 14:44:41 -0800

    make emacs 31 happier with keybindings [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 15171]

--- modes/terminal.el
+++ modes/terminal.el
@@ -1,4 +1,6 @@
+;; -*- lexical-binding: t; -*-
+
 (unless window-system
   (keymap-global-unset "C-z")
   (keymap-global-set (kbd "C-z C-z") 'suspend-frame)
-  (keymap-global-set (kbd "C-Z") 'suspend-frame)) ; for shell-mode in terms
+  (keymap-global-set (kbd "C-S-z") 'suspend-frame)) ; for shell-mode in terms:
