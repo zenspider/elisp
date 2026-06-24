8ab37ad0e0a47b092ac37e47fe8077653e3e70ac
Ryan Davis <ryand-ruby@zenspider.com>
2026-06-23 23:49:42 -0800

    clean up warnings/deprecations in hooks/flymake-mode.el [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 15174]

--- hooks/flymake-mode.el
+++ hooks/flymake-mode.el
@@ -1,12 +1,17 @@
+;; -*- lexical-binding: t; -*-
+
+(eval-and-compile
+  (require 'flymake-proc))
+
 (defun flymake-html-init ()
-  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+  (let* ((temp-file (flymake-proc-init-create-temp-buffer-copy
                      'flymake-create-temp-inplace))
          (local-file (file-relative-name
                       temp-file
                       (file-name-directory buffer-file-name))))
     (list "/usr/local/bin/tidy" (list local-file))))
 
-(add-to-list 'flymake-allowed-file-name-masks
+(add-to-list 'flymake-proc-allowed-file-name-masks
              '(("\\.html\\|\\.erb" flymake-html-init)))
 
 (add-to-list 'flymake-err-line-patterns
