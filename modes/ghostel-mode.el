4b13ad1fb511bb11f6dae3df7b09e0add2d94258
Ryan Davis <ryand-ruby@zenspider.com>
2026-08-18 16:39:07 -0800

    ongoing enhancements to ghostel-mode [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 15234]

--- modes/ghostel-mode.el
+++ modes/ghostel-mode.el
@@ -5,6 +5,16 @@
 (eval-when-compile
   (require 'ghostel))
 
-(with-eval-after-load 'ghostel-mode
+(with-eval-after-load 'ghostel
+  (defun ghostel-send-C-u ()
+    "Send C-u, geneally clearing from point to the prompt."
+    (interactive)
+    (ghostel--on-user-input)
+    (ghostel-send-key "u" "ctrl"))
+
   (keymap-set ghostel-mode-map "C-c e" #'ghostel-clear-scrollback)
+  (keymap-set ghostel-mode-map "C-c C-u" #'ghostel-send-C-u)
+  (keymap-set ghostel-mode-map "C-c u"   #'ghostel-send-C-u)
+  (keymap-set ghostel-mode-map "C-x C-p" #'ghostel-find-file-at-point)
+
   (defalias 'ghostel-write-file #'comint-write-output))
