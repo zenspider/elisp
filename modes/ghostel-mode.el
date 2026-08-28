a38f33621232ace6070545473d17040c88ad1b41
Ryan Davis <ryand-ruby@zenspider.com>
2026-08-28 15:08:25 -0800

    Added some enhancements to ghostel to ease my pain. [git-p4: depot-paths = "//usr/ryand/Bin/elisp/": change = 15254]

--- modes/ghostel-mode.el
+++ modes/ghostel-mode.el
@@ -5,14 +5,40 @@
 (eval-when-compile
   (require 'ghostel))
 
-(with-eval-after-load 'ghostel
-  (defun ghostel-send-C-u ()
-    "Send C-u, geneally clearing from point to the prompt."
-    (interactive)
-    (ghostel--on-user-input)
-    (ghostel-send-key "u" "ctrl"))
+(defun ghostel--keymap-set (key fn)
+  ;; Input modes (`ghostel-semi-char-mode-map', `ghostel-char-mode-map',
+  ;; `ghostel-readonly-mode-map', `ghostel-readonly-fast-exit-mode-map',
+  ;; `ghostel-line-mode-map') inherit or extend this map."
+  (dolist (map (list ghostel-mode-map ghostel-semi-char-mode-map))
+    (message "setting %s to %s" key fn)
+    (keymap-set map key fn)))
 
-  (keymap-set ghostel-mode-map "C-c e" #'ghostel-clear-scrollback)
+(defun ghostel-send-C-u ()
+  "Send C-u, geneally clearing from point to the prompt."
+  (interactive)
+  (ghostel--on-user-input)
+  (ghostel-send-key "u" "ctrl"))
+
+(defun ghostel-bob ()
+  (interactive)
+  (ghostel-emacs-mode)                  ; you're triggering this to WATCH it run
+  (goto-char (point-min)))
+
+(defun ghostel-eob ()
+  (interactive)
+  (ghostel-readonly-exit))
+
+(defun ghostel-kill-line ()
+  (interactive)
+  (kill-line))
+
+(with-eval-after-load 'ghostel
+  ;; (add-to-list 'ghostel-keymap-exceptions "C-k" t)
+  ;; (keymap-set ghostel-mode-map "C-k" #'kill-line)
+  (ghostel--keymap-set         "C-k"     #'ghostel-kill-line)
+  (ghostel--keymap-set         "M-<"     #'ghostel-bob)
+  (ghostel--keymap-set         "M->"     #'ghostel-eob)
+  (keymap-set ghostel-mode-map "C-c e"   #'ghostel-clear-scrollback)
   (keymap-set ghostel-mode-map "C-c C-u" #'ghostel-send-C-u)
   (keymap-set ghostel-mode-map "C-c u"   #'ghostel-send-C-u)
   (keymap-set ghostel-mode-map "C-x C-p" #'ghostel-find-file-at-point)
